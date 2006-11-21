a5dd5bc78ded8f69a9b7d9692dbf343ae21525fa
Andreas Krennmair <ak@synflood.at>
2006-11-21 06:18:01 +0000

    Andreas Krennmair: 	added missing config.h header.

---
+++ config.h
@@ -0,0 +1,12 @@
+#ifndef NOOS_CONFIG__H
+#define NOOS_CONFIG__H
+
+#define PROGRAM_NAME		"noos"
+#define PROGRAM_VERSION		"0.1"
+#define PROGRAM_URL			"http://future.noos.homepage.invalid/"
+#define USER_AGENT			PROGRAM_NAME " rss feedreader " PROGRAM_VERSION " (" PROGRAM_URL ")"
+
+#define NOOS_PATH_SEP		"/"
+#define NOOS_CONFIG_SUBDIR	".noos"
+
+#endif
