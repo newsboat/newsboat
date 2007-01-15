26e26a0f72d454c6705131507109020e5b15671d
Andreas Krennmair <ak@synflood.at>
2007-01-15 22:01:20 +0000

    Andreas Krennmair: 	tagged noos 0.2

--- config.h
+++ config.h
@@ -2,8 +2,8 @@
 #define NOOS_CONFIG__H
 
 #define PROGRAM_NAME		"noos"
-#define PROGRAM_VERSION		"0.1"
-#define PROGRAM_URL			"http://future.noos.homepage.invalid/"
+#define PROGRAM_VERSION		"0.2"
+#define PROGRAM_URL			"http://synflood.at/noos.html"
 #define USER_AGENT			PROGRAM_NAME " rss feedreader " PROGRAM_VERSION " (" PROGRAM_URL ")"
 
 #define NOOS_PATH_SEP		"/"
