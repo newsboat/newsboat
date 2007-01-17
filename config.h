2dad20e4c7d4472f424880da85589099cce000db
Andreas Krennmair <ak@synflood.at>
2007-01-17 21:32:33 +0000

    Andreas Krennmair: 	tagged newsbeuter 0.1.1.

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 #define NEWSBEUTER_CONFIG__H
 
 #define PROGRAM_NAME		"newsbeuter"
-#define PROGRAM_VERSION		"0.1"
+#define PROGRAM_VERSION		"0.1.1"
 #define PROGRAM_URL			"http://synflood.at/newsbeuter.html"
 #define USER_AGENT			PROGRAM_NAME " rss feedreader " PROGRAM_VERSION " (" PROGRAM_URL ")"
 
