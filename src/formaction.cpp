d869ecf448b70ac6fea5fc2d2e23361d91ac3695
Andreas Krennmair <ak@synflood.at>
2007-05-21 20:20:44 +0000

    Andreas Krennmair: 	implemented a simple "set" function for the commandline.

--- src/formaction.cpp
+++ src/formaction.cpp
@@ -1,5 +1,7 @@
 #include <formaction.h>
 #include <view.h>
+#include <utils.h>
+#include <config.h>
 
 namespace newsbeuter {
 
@@ -34,8 +36,26 @@ std::string formaction::get_value(const std::string& value) {
 	return f->get(value);
 }
 
-void formaction::handle_cmdline(const std::string& /*cmd*/) {
-	// TODO: handle generic variable get/set operations
+void formaction::handle_cmdline(const std::string& cmdline) {
+	std::vector<std::string> tokens = utils::tokenize_quoted(cmdline, " \t=");
+	char buf[1024];
+	configcontainer * cfg = v->get_cfg();
+	assert(cfg != NULL);
+	if (tokens.size() > 0) {
+		std::string cmd = tokens[0];
+		tokens.erase(tokens.begin());
+		if (cmd == "set") {
+			if (tokens.size()==0) {
+				v->show_error(_("usage: set <variable>[=<value>]"));
+			} else if (tokens.size()==1) {
+				snprintf(buf,sizeof(buf), "  %s=%s", tokens[0].c_str(), cfg->get_configvalue(tokens[0]).c_str());
+			} else if (tokens.size()==2) {
+				cfg->set_configvalue(tokens[0], tokens[1]);
+			} else {
+				v->show_error(_("usage: set <variable>[=<value>]"));
+			}
+		}
+	}
 }
 
 }
