90fd0e17fd78948218970c5a5424d07340fbedd2
Andreas Krennmair <ak@synflood.at>
2007-08-03 08:33:35 +0000

    Andreas Krennmair: 	fix "~" resolving when PATH values never got set in the configuration.

--- src/configcontainer.cpp
+++ src/configcontainer.cpp
@@ -85,47 +85,13 @@ action_handler_status configcontainer::handle_action(const std::string& action, 
 			return AHS_OK;	
 
 		case configdata::STR:
+		case configdata::PATH:
 			if (params.size() < 1) {
 				return AHS_TOO_FEW_PARAMS;
 			}
 			cfgdata.value = params[0];
 			return AHS_OK;	
 
-		case configdata::PATH: {
-			if (params.size() < 1) {
-				return AHS_TOO_FEW_PARAMS;
-			}
-
-			// a path config option is a bit more difficult to handle, because we need to replace 
-			// a possible "~/" at the beginning of the string with the user's home directory.
-
-			const char * homedir;
-			std::string filepath;
-
-			if (!(homedir = ::getenv("HOME"))) {
-				struct passwd * spw = ::getpwuid(::getuid());
-				if (spw) {
-						homedir = spw->pw_dir;
-				} else {
-						homedir = "";
-				}
-			}
-
-			if (strcmp(homedir,"")!=0) {
-				if (params[0] == "~") {
-					filepath.append(homedir);
-				} else if (params[0].substr(0,2) == "~/") {
-					filepath.append(homedir);
-					filepath.append(1,'/');
-					filepath.append(params[0].substr(2,params[0].length()-2));
-				}
-			} else {
-				filepath.append(params[0]);
-			}
-
-			cfgdata.value = filepath;
-			return AHS_OK;
-		}
 		default:
 			// should not happen
 			return AHS_INVALID_COMMAND;	
@@ -154,7 +120,35 @@ bool configcontainer::is_int(const std::string& s) {
 }
 
 std::string configcontainer::get_configvalue(const std::string& key) {
-	return config_data[key].value;
+	std::string retval = config_data[key].value;
+	if (config_data[key].type == configdata::PATH) {
+		const char * homedir;
+		std::string filepath;
+
+		if (!(homedir = ::getenv("HOME"))) {
+			struct passwd * spw = ::getpwuid(::getuid());
+			if (spw) {
+					homedir = spw->pw_dir;
+			} else {
+					homedir = "";
+			}
+		}
+
+		if (strcmp(homedir,"")!=0) {
+			if (retval == "~") {
+				filepath.append(homedir);
+			} else if (retval.substr(0,2) == "~/") {
+				filepath.append(homedir);
+				filepath.append(1,'/');
+				filepath.append(retval.substr(2,retval.length()-2));
+			}
+		} else {
+			filepath.append(retval);
+		}
+		retval = filepath;
+	}
+
+	return retval;
 }
 
 int configcontainer::get_configvalue_as_int(const std::string& key) {
