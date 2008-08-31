f727196cdb70ce36efd17167911beff8f35edf74
Andreas Krennmair <ak@synflood.at>
2008-08-31 12:10:30 +0000

    Andreas Krennmair: 	added UI test to open all dialogs.

---
+++ test/test-openalldialog.rb
@@ -0,0 +1,67 @@
+#!/usr/bin/env ruby
+# auto-generated tuitest script
+require 'tuitest'
+
+Kernel.system("rm -f cache")
+
+Tuitest.init
+verifier = Tuitest::Verifier.new("test-openalldialog.rb.log")
+
+
+Tuitest.run("../newsbeuter -c cache -C /dev/null -u urls-tuitest1")
+
+Tuitest.wait_until_idle
+verifier.expect(0, 17, "Your feeds (0 unread, 1 total)")
+
+Tuitest.keypress("r"[0])
+Tuitest.keypress(10)
+
+Tuitest.wait_until_idle
+verifier.expect(0, 17, "Articles in feed 'RSS 2.0 testbed feed' (3 unread, 3 total) - h")
+
+Tuitest.keypress(258)
+Tuitest.keypress(258)
+Tuitest.keypress(10)
+
+Tuitest.wait_until_idle
+verifier.expect(0, 17, "Article ")
+
+Tuitest.keypress("?"[0])
+
+Tuitest.wait_until_idle
+verifier.expect(0, 17, "Help                    ")
+
+Tuitest.keypress("q"[0])
+Tuitest.keypress("u"[0])
+
+Tuitest.wait_until_idle
+verifier.expect(0, 17, "URLs")
+
+Tuitest.keypress("q"[0])
+Tuitest.keypress("u"[0])
+Tuitest.keypress("q"[0])
+Tuitest.keypress("s"[0])
+
+# begin auto-generated verification #6 
+Tuitest.wait_until_idle
+verifier.expect(0, 17, "Save File - ")
+
+Tuitest.keypress(259)
+Tuitest.keypress("q"[0])
+Tuitest.keypress("q"[0])
+Tuitest.keypress("q"[0])
+Tuitest.keypress("t"[0])
+
+Tuitest.wait_until_idle
+verifier.expect(0, 17, "Select Tag")
+
+Tuitest.keypress("q"[0])
+Tuitest.keypress("q"[0])
+Tuitest.wait_until_idle
+
+Tuitest.close
+verifier.finish
+
+Kernel.system("rm -f cache")
+
+# EOF
