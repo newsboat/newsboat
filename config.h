ccb4f0d9ba81b235faf55dcebe61b744dc033c78
Andreas Krennmair <ak@synflood.at>
2008-09-02 11:59:06 +0000

    Andreas Krennmair: 	merged & updated changelog 	updated version number

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 #define NEWSBEUTER_CONFIG__H
 
 #define PROGRAM_NAME		PACKAGE
-#define PROGRAM_VERSION		"1.2"
+#define PROGRAM_VERSION		"1.3"
 #define PROGRAM_URL			"http://www.newsbeuter.org/"
 #define USER_AGENT			PROGRAM_NAME " rss feedreader " PROGRAM_VERSION " (" PROGRAM_URL ")"
 
