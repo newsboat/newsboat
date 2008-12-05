d6d39293c9a3db836d0cfb183bfbb51918353354
Andreas Krennmair <ak@synflood.at>
2008-12-05 16:21:15 +0000

    Andreas Krennmair: 	crash fix. 	removed old file.

--- src/view.cpp
+++ src/view.cpp
@@ -458,7 +458,7 @@ char view::confirm(const std::string& prompt, const std::string& charset) {
 
 void view::notify_itemlist_change(std::tr1::shared_ptr<rss_feed>& feed) {
 	for (std::vector<std::tr1::shared_ptr<formaction> >::iterator it=formaction_stack.begin();it!=formaction_stack.end();it++) {
-		if ((*it)->id() == "articlelist") {
+		if (*it != NULL && (*it)->id() == "articlelist") {
 			std::tr1::shared_ptr<itemlist_formaction> itemlist = std::tr1::dynamic_pointer_cast<itemlist_formaction, formaction>(*it);
 			if (itemlist != NULL) {
 				std::tr1::shared_ptr<rss_feed> f = itemlist->get_feed();
