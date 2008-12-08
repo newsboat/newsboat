60c17d3337c9125d17f34aadbceac24cbd77fc49
Andreas Krennmair <ak@synflood.at>
2008-12-08 07:54:12 +0000

    Andreas Krennmair: 	updated patch from issue #102.

--- xmlrss/mrss_parser.c
+++ xmlrss/mrss_parser.c
@@ -373,10 +373,18 @@ __mrss_parser_atom_entry (nxml_t * doc, nxml_data_t * cur, mrss_t * data)
 
 	  /* link href -> link */
 	  else if (!item->link && !strcmp (cur->value, "link") 
-		   && (t = nxmle_find_attribute (cur, "rel", NULL))
-		   && !strcmp (t, "alternate")
-		   && (c = nxmle_find_attribute (cur, "href", NULL)))
-	    item->link = (char *)c;
+		   && (t = nxmle_find_attribute (cur, "href", NULL)))
+	    {
+		const char *t;
+		
+		/* alternate link is either rel="alternate" or a link tag
+		 * without a rel attribute
+		 */
+		t = nxmle_find_attribute (cur, "rel", NULL);
+		if ((t && !strcmp(t, "alternate")) || !t)
+		  item->link = c;
+	    }
+
 
 	  /* content -> description */
 	  else if (!item->description && !strcmp (cur->value, "content"))
