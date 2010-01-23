90dded8768edea1d62cb0c6150debed5bff7db7a
Andreas Krennmair <ak@synflood.at>
2010-01-23 13:58:14 +0100

    fixed RSS 1.0 parser.
    OMG, why all the basic feed parsing breakage all of the sudden?

--- rss/rss_10_parser.cpp
+++ rss/rss_10_parser.cpp
@@ -6,6 +6,8 @@
 #include <rsspp_internal.h>
 #include <cstring>
 
+#define RSS_1_0_NS "http://purl.org/rss/1.0/"
+
 namespace rsspp {
 
 void rss_10_parser::parse_feed(feed& f, xmlNode * rootNode) {
@@ -13,14 +15,14 @@ void rss_10_parser::parse_feed(feed& f, xmlNode * rootNode) {
 		throw exception(_("XML root node is NULL"));
 
 	for (xmlNode * node = rootNode->children; node != NULL; node = node->next) {
-		if (node_is(node, "channel")) {
+		if (node_is(node, "channel", RSS_1_0_NS)) {
 			for (xmlNode * cnode = node->children; cnode != NULL; cnode = cnode->next) {
-				if (node_is(cnode, "title")) {
+				if (node_is(cnode, "title", RSS_1_0_NS)) {
 					f.title = get_content(cnode);
 					f.title_type = "text";
-				} else if (node_is(cnode, "link")) {
+				} else if (node_is(cnode, "link", RSS_1_0_NS)) {
 					f.link = get_content(cnode);
-				} else if (node_is(cnode, "description")) {
+				} else if (node_is(cnode, "description", RSS_1_0_NS)) {
 					f.description = get_content(cnode);
 				} else if (node_is(cnode, "date", DC_URI)) {
 					f.pubDate = w3cdtf_to_rfc822(get_content(cnode));
@@ -28,16 +30,16 @@ void rss_10_parser::parse_feed(feed& f, xmlNode * rootNode) {
 					f.dc_creator = get_content(cnode);
 				}
 			}
-		} else if (node_is(node, "item")) {
+		} else if (node_is(node, "item", RSS_1_0_NS)) {
 			item it;
 			it.guid = get_prop(node, "about", RDF_URI);
 			for (xmlNode * itnode = node->children; itnode != NULL; itnode = itnode->next) {
-				if (node_is(itnode, "title")) {
+				if (node_is(itnode, "title", RSS_1_0_NS)) {
 					it.title = get_content(itnode);
 					it.title_type = "text";
 				} else if (node_is(itnode, "link")) {
 					it.link = get_content(itnode);
-				} else if (node_is(itnode, "description")) {
+				} else if (node_is(itnode, "description", RSS_1_0_NS)) {
 					it.description = get_content(itnode);
 				} else if (node_is(itnode, "date", DC_URI)) {
 					it.pubDate = w3cdtf_to_rfc822(get_content(itnode));
