711c8f7d0246ec7a95dbe6ca96da94d05e9dba73
Andreas Krennmair <ak@synflood.at>
2010-03-21 15:16:32 +0100

    update code coverage data after each partial test run.

--- test/lcov-run-all.sh
+++ test/lcov-run-all.sh
@@ -9,10 +9,11 @@ rm -rf $APPBASE_INFO $APPTEST_INFO html
 make -j 5 -f Makefile.prof all test test-rss
 lcov -c -i -b . -d . -o $APPBASE_INFO
 export OFFLINE=1
-( cd test
-./test
-./test-rss
-./run-uitests-headless.sh )
+( cd test && ./test )
+lcov -c -b . -d . -o $APPTEST_INFO
+( cd test && ./test-rss )
+lcov -c -b . -d . -o $APPTEST_INFO
+( cd test && ./run-uitests-headless.sh )
 lcov -c -b . -d . -o $APPTEST_INFO
 lcov -b . -d . -a $APPBASE_INFO -a $APPTEST_INFO -o $APPTOTAL_INFO
 rm -rf html
