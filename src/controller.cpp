5b98407f3d843ea9fcf4896701e5b3647bd479ac
Andreas Krennmair <ak@synflood.at>
2010-09-14 18:03:38 +0200

    streamlined patch for XDG Base Directory support (issue #86).

--- src/controller.cpp
+++ src/controller.cpp
@@ -91,7 +91,6 @@ bool controller::setup_dirs_xdg(const char *env_home) {
 	const char *env_xdg_data;
 	std::string xdg_config_dir;
 	std::string xdg_data_dir;
-	std::string data_dir;
 
 	env_xdg_config = ::getenv("XDG_CONFIG_HOME");
 	if (env_xdg_config) {
@@ -101,7 +100,6 @@ bool controller::setup_dirs_xdg(const char *env_home) {
 		xdg_config_dir.append(NEWSBEUTER_PATH_SEP);
 		xdg_config_dir.append(".config");
 	}
-	std::cout << "xdg config: " << xdg_config_dir << std::endl;
 
 	env_xdg_data = ::getenv("XDG_DATA_HOME");
 	if (env_xdg_data) {
@@ -113,39 +111,36 @@ bool controller::setup_dirs_xdg(const char *env_home) {
 		xdg_data_dir.append(NEWSBEUTER_PATH_SEP);
 		xdg_data_dir.append("share");
 	}
-	// std::cout << "xdg data: " << xdg_data_dir << std::endl;
+
+	xdg_config_dir.append(NEWSBEUTER_PATH_SEP);
+	xdg_config_dir.append(NEWSBEUTER_SUBDIR_XDG);
+
+	xdg_data_dir.append(NEWSBEUTER_PATH_SEP);
+	xdg_data_dir.append(NEWSBEUTER_SUBDIR_XDG);
+
+	if (access(xdg_config_dir.c_str(), R_OK | X_OK) != 0)
+	{
+		std::cout << utils::strprintf(_("XDG: configuration directory '%s' not accessible, using '%s' instead."), xdg_config_dir.c_str(), config_dir.c_str()) << std::endl;
+		return false;
+	}
+	if (access(xdg_data_dir.c_str(), R_OK | X_OK | W_OK) != 0)
+	{
+		std::cout << utils::strprintf(_("XDG: data directory '%s' not accessible, using '%s' instead."), xdg_data_dir.c_str(), config_dir.c_str()) << std::endl;
+		return false;
+	}
 
 	config_dir = xdg_config_dir;
-	config_dir.append(NEWSBEUTER_PATH_SEP);
-	config_dir.append(NEWSBEUTER_SUBDIR_XDG);
-
-	data_dir = xdg_data_dir;
-	data_dir.append(NEWSBEUTER_PATH_SEP);
-	data_dir.append(NEWSBEUTER_SUBDIR_XDG);
-
-	if (access(config_dir.c_str(), R_OK | X_OK) != 0)
-	{
-		std::cout << "XDG: Directory not accessible, going non-XDG: "
-			<< config_dir << std::endl;
-		return false;
-	}
-	if (access(data_dir.c_str(), R_OK | X_OK | W_OK) != 0)
-	{
-		std::cout << "XDG: Directory not accessible, going non-XDG: "
-			<< data_dir << std::endl;
-		return false;
-	}
 
 	/* in config */
 	url_file = config_dir + std::string(NEWSBEUTER_PATH_SEP) + url_file;
 	config_file = config_dir + std::string(NEWSBEUTER_PATH_SEP) + config_file;
 
 	/* in data */
-	cache_file = data_dir + std::string(NEWSBEUTER_PATH_SEP) + cache_file;
+	cache_file = xdg_data_dir + std::string(NEWSBEUTER_PATH_SEP) + cache_file;
 	lock_file = cache_file + LOCK_SUFFIX;
 	queue_file = config_dir + std::string(NEWSBEUTER_PATH_SEP) + queue_file;
-	searchfile = utils::strprintf("%s%shistory.search", data_dir.c_str(), NEWSBEUTER_PATH_SEP);
-	cmdlinefile = utils::strprintf("%s%shistory.cmdline", data_dir.c_str(), NEWSBEUTER_PATH_SEP);
+	searchfile = utils::strprintf("%s%shistory.search", xdg_data_dir.c_str(), NEWSBEUTER_PATH_SEP);
+	cmdlinefile = utils::strprintf("%s%shistory.cmdline", xdg_data_dir.c_str(), NEWSBEUTER_PATH_SEP);
 
 	return true;
 }
@@ -163,12 +158,14 @@ void controller::setup_dirs() {
 		}
 	}
 
-	if (setup_dirs_xdg(env_home))
-		return;
 
 	config_dir = env_home;
 	config_dir.append(NEWSBEUTER_PATH_SEP);
 	config_dir.append(NEWSBEUTER_CONFIG_SUBDIR);
+
+	if (setup_dirs_xdg(env_home))
+		return;
+
 	mkdir(config_dir.c_str(),0700); // create configuration directory if it doesn't exist
 
 	url_file = config_dir + std::string(NEWSBEUTER_PATH_SEP) + url_file;
