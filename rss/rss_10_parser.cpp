f961434118b3bf772e4597173893705521691735
Andreas Krennmair <ak@synflood.at>
2012-01-06 15:38:59 +0100

    2011 -> 2012.

--- rss/rss_10_parser.cpp
+++ rss/rss_10_parser.cpp
@@ -1,4 +1,4 @@
-/* rsspp - Copyright (C) 2008-2011 Andreas Krennmair <ak@newsbeuter.org> 
+/* rsspp - Copyright (C) 2008-2012 Andreas Krennmair <ak@newsbeuter.org> 
  * Licensed under the MIT/X Consortium License. See file LICENSE 
  * for more information. 
  */
