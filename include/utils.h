b6957af774a0cb49fcb08c7e25cdafb1e8feed3e
Martin Perner <martin@perner.cc>
2012-12-09 01:23:17 +0100

    TinyTinyRSS API update
    Changed the API used by TinyTinyRSS from the obsolete HTTP/GET to JSON/POST.

--- include/utils.h
+++ include/utils.h
@@ -50,7 +50,7 @@ class utils {
 
 		static std::string get_command_output(const std::string& cmd);
 		static void extract_filter(const std::string& line, std::string& filter, std::string& url);
-		static std::string retrieve_url(const std::string& url, configcontainer * cfgcont = NULL, const char * authinfo = NULL);
+		static std::string retrieve_url(const std::string& url, configcontainer * cfgcont = NULL, const char * authinfo = NULL, const std::string* postdata = NULL);
 		static void run_command(const std::string& cmd, const std::string& param); // used for notifications only
 		static std::string run_program(char * argv[], const std::string& input);
 
