9f906b2411a0bda9b5faf4d29e2b3def18d2c7d9
Giuliano Schneider <gs93@gmx.net>
2013-03-28 12:58:25 +0100

    fixed typo

--- src/itemlist_formaction.cpp
+++ src/itemlist_formaction.cpp
@@ -134,7 +134,7 @@ void itemlist_formaction::process_operation(operation op, bool automatic, std::v
 					} catch (const dbexception& e) {
 						v->set_status(utils::strprintf(_("Error while toggling read flag: %s"), e.what()));
 					}
-					if (!v->get_cfg()->get_configvalue_as_bool("toogleitemread-jumps-to-next-unread")) {
+					if (!v->get_cfg()->get_configvalue_as_bool("toggleitemread-jumps-to-next-unread")) {
 						if (itempos < visible_items.size()-1)
 							f->set("itempos", utils::strprintf("%u", itempos + 1));
 					} else {
