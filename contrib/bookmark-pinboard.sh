68d09525282b8ca8395111fda6df99a73debe2bb
Larry Hynes <larryhynes@users.noreply.github.com>
2014-07-24 21:49:55 +0100

    Commented tags and added toread flag

--- contrib/bookmark-pinboard.sh
+++ contrib/bookmark-pinboard.sh
@@ -6,13 +6,17 @@
 username="pinboard_username"
 password="pinboard_password"
 
-tag="via:newsbeuter"
+# You can enter up to 100 tags here, space delimited, or leave blank for no tag
+tags="via:newsbeuter"
+
+# Leave as 'yes' to bookmark as to be read, enter 'no' to mark as read
+toread="yes"
 
 url="$1"
 title="$2"
 desc="$3"
 
-pinboard_url="https://api.pinboard.in/v1/posts/add?url=${url}&description=${title}&extended=${desc}&tags="${tag}""
+pinboard_url="https://api.pinboard.in/v1/posts/add?url=${url}&description=${title}&extended=${desc}&tags="${tags}"&toread=${toread}"
 
 output=`wget --http-user=$username --http-passwd=$password -O - "$pinboard_url" 2> /dev/null`
 
