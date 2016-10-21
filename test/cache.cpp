a4a61d30fcc4798aa7f158097e07b0ee877961dd
Alexander Batischev <eual.jp@gmail.com>
2016-10-21 21:09:38 +0300

    Test cache::{update,fetch}_lastmodified

--- test/cache.cpp
+++ test/cache.cpp
@@ -75,3 +75,40 @@ TEST_CASE("Cleaning old articles works") {
 	/* The important part: old articles should be gone, new one remains. */
 	REQUIRE(feed->items().size() == 1);
 }
+
+TEST_CASE("Last-Modified and ETag values are preserved correctly") {
+	configcontainer cfg;
+	cache rsscache(":memory:", &cfg);
+	const auto feedurl = "file://data/rss.xml";
+	rss_parser parser(feedurl, &rsscache, &cfg, nullptr);
+	std::shared_ptr<rss_feed> feed = parser.parse();
+	rsscache.externalize_rssfeed(feed, false);
+
+	/* We will run this lambda on different inputs to check different
+	 * situations. */
+	auto test = [&](const time_t& lm_value, const std::string& etag_value) {
+		time_t last_modified = lm_value;
+		std::string etag = etag_value;
+
+		rsscache.update_lastmodified(feedurl, last_modified, etag);
+		/* Scrambling the value to make sure the following call changes it. */
+		last_modified = 42;
+		etag = "42";
+		rsscache.fetch_lastmodified(feedurl, last_modified, etag);
+
+		REQUIRE(last_modified == lm_value);
+		REQUIRE(etag == etag_value);
+	};
+
+	SECTION("Only Last-Modified header was returned") {
+		test(1476382350, "");
+	}
+
+	SECTION("Only ETag header was returned") {
+		test(0, "1234567890");
+	}
+
+	SECTION("Both Last-Modified and ETag headers were returned") {
+		test(1476382350, "1234567890");
+	}
+}
