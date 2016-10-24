e8b9a7d63a69302feaa02609944ec5f30bbfa24a
Alexander Batischev <eual.jp@gmail.com>
2016-10-24 19:38:17 +0300

    Replace bare pointers with stack allocation in tests

--- test/keymap.cpp
+++ test/keymap.cpp
@@ -21,6 +21,7 @@ TEST_CASE("keymap::get_operation()") {
 
 TEST_CASE("keymap::unset_key() and set_key()") {
 	keymap k(KM_NEWSBEUTER);
+
 	REQUIRE(k.get_operation("ENTER", "feedlist") == OP_OPEN);
 	REQUIRE(k.getkey(OP_OPEN, "all") == "ENTER");
 
