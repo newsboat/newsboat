30289140b9325389511d41f7460b18c02eb15912
Alexander Batischev <eual.jp@gmail.com>
2016-10-27 22:05:17 +0300

    Remove char* in rsspp::parser

--- rss/rsspp.h
+++ rss/rsspp.h
@@ -73,7 +73,13 @@ class exception : public std::exception {
 
 class parser {
 	public:
-		parser(unsigned int timeout = 30, const char * user_agent = 0, const char * proxy = 0, const char * proxy_auth = 0, curl_proxytype proxy_type = CURLPROXY_HTTP, const bool ssl_verify = true);
+		parser(
+				unsigned int timeout = 30,
+				const std::string& user_agent = "",
+				const std::string& proxy = "",
+				const std::string& proxy_auth = "",
+				curl_proxytype proxy_type = CURLPROXY_HTTP,
+				const bool ssl_verify = true);
 		~parser();
 		feed parse_url(const std::string& url, time_t lastmodified = 0, const std::string& etag = "", newsbeuter::remote_api * api = 0, const std::string& cookie_cache = "", CURL *ehandle = 0);
 		feed parse_buffer(const std::string& buffer, const std::string& url = "");
@@ -91,9 +97,9 @@ class parser {
 
 		feed parse_xmlnode(xmlNode * node);
 		unsigned int to;
-		const char * ua;
-		const char * prx;
-		const char * prxauth;
+		const std::string ua;
+		const std::string prx;
+		const std::string prxauth;
 		curl_proxytype prxtype;
 		const bool verify_ssl;
 		xmlDocPtr doc;
