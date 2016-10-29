edd8420f71a12e134a4bdd2685d440df3c153289
Alexander Batischev <eual.jp@gmail.com>
2016-10-30 02:10:10 +0300

    Reformat utils::make_title

--- src/utils.cpp
+++ src/utils.cpp
@@ -1021,14 +1021,23 @@ std::string utils::make_title(const std::string& const_url) {
 	 * This attempts to do just that. eg: http://domain.com/story/yy/mm/dd/title-with-dashes?a=b
 	*/
 	std::string url = (std::string&) const_url;
-	std::string::size_type pos_of_slash = url.find_last_of('/', (int)url.length()-2); 		// get to the final part of the URI's path, catering for situation where last char is '/'
+	// get to the final part of the URI's path, catering for situation where
+	// last char is '/'
+	std::string::size_type pos_of_slash = url.find_last_of('/', (int)url.length()-2);
 	if (url[url.length()-1] == '/')
 		url.erase((int)url.length()-1);
-	std::string path=url.substr(pos_of_slash+1); 				// extract just the juicy part 'title-with-dashes?a=b'
-	std::string::size_type pos_of_qmrk = path.find_first_of('?'); 		// find where query part of URI starts
-	std::string title = path.substr(0,pos_of_qmrk); 			//throw away the query part 'title-with-dashes'
-	std::replace(title.begin(), title.end(), '-', ' ');			// 'title with dashes'
-	if (title.at(0)>= 'a' && title.at(0)<= 'z') title[0] -= 'a' - 'A';	//'Title with dashes'
+	// extract just the juicy part 'title-with-dashes?a=b'
+	std::string path = url.substr(pos_of_slash+1);
+	// find where query part of URI starts
+	std::string::size_type pos_of_qmrk = path.find_first_of('?');
+	//throw away the query part 'title-with-dashes'
+	std::string title = path.substr(0,pos_of_qmrk);
+	// 'title with dashes'
+	std::replace(title.begin(), title.end(), '-', ' ');
+	//'Title with dashes'
+	if (title.at(0)>= 'a' && title.at(0)<= 'z') {
+		title[0] -= 'a' - 'A';
+	}
 	return title;
 }
 
