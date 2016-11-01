newsboat/newsboat

commit afd5ec5266b90d7bb8adac8d4ff793a89de0daa4

Use range-based for loops wherever possible

src/pb_controller.cpp
@@ -340,9 +340,9 @@ double pb_controller::get_total_kbps() {
 
 void pb_controller::start_downloads() {
 	int dl2start = get_maxdownloads() - downloads_in_progress();
-	for (auto it=downloads_.begin(); dl2start > 0 && it!=downloads_.end(); ++it) {
-		if (it->status() == DL_QUEUED) {
-			std::thread t {poddlthread(&(*it), cfg)};
+	for (auto& download : downloads_) {
+		if (download.status() == DL_QUEUED) {
+			std::thread t {poddlthread(&download, cfg)};
 			--dl2start;
 			t.detach();
 		}

src/feedhq_api.cpp
@@ -103,11 +103,10 @@ std::string feedhq_api::retrieve_auth() {
 	curl_easy_perform(handle);
 	curl_easy_cleanup(handle);
 
-	std::vector<std::string> lines = utils::tokenize(result);
-	for (std::vector<std::string>::iterator it=lines.begin(); it!=lines.end(); ++it) {
-		LOG(LOG_DEBUG, "feedhq_api::retrieve_auth: line = %s", it->c_str());
-		if (it->substr(0,5)=="Auth=") {
-			std::string auth = it->substr(5, it->length()-5);
+	for (const auto& line : utils::tokenize(result)) {
+		LOG(LOG_DEBUG, "feedhq_api::retrieve_auth: line = %s", line.c_str());
+		if (line.substr(0,5) == "Auth=") {
+			std::string auth = line.substr(5, line.length()-5);
 			return auth;
 		}
 	}

src/feedhq_urlreader.cpp
@@ -38,26 +38,29 @@ void feedhq_urlreader::reload() {
 	file_urlreader ur(file);
 	ur.reload();
 
-	std::vector<std::string>& file_urls(ur.get_urls());
-	for(std::vector<std::string>::iterator it=file_urls.begin(); it!=file_urls.end(); ++it) {
-		if (it->substr(0,6) == "query:") {
-			urls.push_back(*it);
-			std::vector<std::string>& file_tags(ur.get_tags(*it));
-			tags[*it] = ur.get_tags(*it);
-			for (std::vector<std::string>::iterator jt=file_tags.begin(); jt!=file_tags.end(); ++jt) {
-				alltags.insert(*jt);
+	for (const auto& url : ur.get_urls()) {
+		if (url.substr(0,6) == "query:") {
+			urls.push_back(url);
+
+			auto url_tags = ur.get_tags(url);
+			tags[url] = url_tags;
+			for (const auto& tag : url_tags) {
+				alltags.insert(tag);
 			}
 		}
 	}
 
 	std::vector<tagged_feedurl> feedurls = api->get_subscribed_urls();
-	for (std::vector<tagged_feedurl>::iterator it=feedurls.begin(); it!=feedurls.end(); ++it) {
-		LOG(LOG_DEBUG, "added %s to URL list", it->first.c_str());
-		urls.push_back(it->first);
-		tags[it->first] = it->second;
-		for (std::vector<std::string>::iterator jt=it->second.begin(); jt!=it->second.end(); ++jt) {
-			LOG(LOG_DEBUG, "%s: added tag %s", it->first.c_str(), jt->c_str());
-			alltags.insert(*jt);
+	for (const auto& tagged : feedurls) {
+		std::string url = tagged.first;
+		std::vector<std::string> url_tags = tagged.second;
+
+		LOG(LOG_DEBUG, "added %s to URL list", url.c_str());
+		urls.push_back(url);
+		tags[tagged.first] = url_tags;
+		for (const auto& tag : url_tags) {
+			LOG(LOG_DEBUG, "%s: added tag %s", url.c_str(), tag.c_str());
+			alltags.insert(tag);
 		}
 	}
 }

commit 7291a1e7604036e1d005b4ffd9a173a0c1041a61

Fix bug introduced in afd5ec5266b90d7bb8adac8d4ff7

src/pb_controller.cpp
@@ -341,6 +341,8 @@ double pb_controller::get_total_kbps() {
 void pb_controller::start_downloads() {
 	int dl2start = get_maxdownloads() - downloads_in_progress();
 	for (auto& download : downloads_) {
+		if (dl2start == 0) break;
+
 		if (download.status() == DL_QUEUED) {
 			std::thread t {poddlthread(&download, cfg)};
 			--dl2start;