newsboat/newsboat

commit 885760cf70c35fb632d2c87216c0eb5d00e1aa8c

Defer text wrapping until the very end (#256)
This lets us handle wrapping in one single place instead of smearing it
all over HTML renderer and listformatter. HTML renderer now signals how
it wants a string formatted—it can be either wrappable or
non-wrappable—and textformatter does the actual wrapping.

include/textformatter.h
@@ -1,0 +1,40 @@
+#ifndef TEXTFORMATTER__H
+#define TEXTFORMATTER__H
+
+#include <climits>
+#include <vector>
+#include <string>
+#include <utility>
+#include <regexmanager.h>
+
+namespace newsbeuter {
+
+enum LineType {
+	wrappable = 1,
+	nonwrappable,
+	hr
+};
+
+class textformatter {
+
+	public:
+		textformatter();
+		~textformatter();
+		void add_line(LineType type, std::string line);
+		void add_lines(
+				const std::vector<std::pair<LineType, std::string>> lines);
+		std::string format_text_to_list(regexmanager * r = NULL,
+			const std::string& location = "",
+			const size_t width = 80);
+		std::string format_text_plain(const size_t width = 80);
+
+		inline void clear() {
+			lines.clear();
+		}
+	private:
+		std::vector<std::pair<LineType, std::string>> lines;
+};
+
+}
+
+#endif

src/itemview_formaction.cpp
@@ -5,7 +5,7 @@
 #include <exceptions.h>
 #include <utils.h>
 #include <formatstring.h>
-#include <listformatter.h>
+#include <textformatter.h>
 #include <cstring>
 
 #include <sstream>
@@ -51,18 +51,8 @@ void itemview_formaction::prepare() {
 			f->run(-3); // XXX HACK: render once so that we get a proper widget width
 		}
 
-		std::vector<std::string> lines;
-		std::string widthstr = f->get("article:w");
-		unsigned int render_width = 80;
-		unsigned int view_width = 0;
-		if (widthstr.length() > 0) {
-			view_width = render_width = utils::to_u(widthstr, 80);
-			if (render_width - 5 > 0)
-				render_width -= 5;
-		}
-
 		std::shared_ptr<rss_item> item = feed->get_item_by_guid(guid);
-		listformatter listfmt;
+		textformatter textfmt;
 
 		std::shared_ptr<rss_feed> feedptr = item->get_feedptr();
 
@@ -78,30 +68,30 @@ void itemview_formaction::prepare() {
 		}
 		if (feedtitle.length() > 0) {
 			feedheader = utils::strprintf("%s%s", _("Feed: "), feedtitle.c_str());
-			listfmt.add_line(feedheader, UINT_MAX, view_width);
+			textfmt.add_line(newsbeuter::wrappable, feedheader);
 		}
 
 		if (item->title().length() > 0) {
 			std::string title = utils::strprintf("%s%s", _("Title: "), item->title().c_str());
-			listfmt.add_line(title, UINT_MAX, view_width);
+			textfmt.add_line(newsbeuter::wrappable, title);
 		}
 
 		if (item->author().length() > 0) {
 			std::string author = utils::strprintf("%s%s", _("Author: "), item->author().c_str());
-			listfmt.add_line(author, UINT_MAX, view_width);
+			textfmt.add_line(newsbeuter::wrappable, author);
 		}
 
 		if (item->link().length() > 0) {
 			std::string link = utils::strprintf("%s%s", _("Link: "), utils::censor_url(item->link()).c_str());
-			listfmt.add_line(link, UINT_MAX, view_width);
+			textfmt.add_line(newsbeuter::nonwrappable, link);
 		}
 
 		std::string date = utils::strprintf("%s%s", _("Date: "), item->pubDate().c_str());
-		listfmt.add_line(date, UINT_MAX, view_width);
+		textfmt.add_line(newsbeuter::wrappable, date);
 
 		if (item->flags().length() > 0) {
 			std::string flags = utils::strprintf("%s%s", _("Flags: "), item->flags().c_str());
-			listfmt.add_line(flags, UINT_MAX, view_width);
+			textfmt.add_line(newsbeuter::wrappable, flags);
 		}
 
 		if (item->enclosure_url().length() > 0) {
@@ -109,10 +99,10 @@ void itemview_formaction::prepare() {
 			if (item->enclosure_type() != "") {
 				enc_url.append(utils::strprintf(" (%s%s)",  _("type: "), item->enclosure_type().c_str()));
 			}
-			listfmt.add_line(enc_url, UINT_MAX, view_width);
+			textfmt.add_line(newsbeuter::nonwrappable, enc_url);
 		}
 
-		listfmt.add_line("");
+		textfmt.add_line(newsbeuter::wrappable, std::string());
 
 		unsigned int unread_item_count = feed->unread_item_count();
 		// we need to subtract because the current item isn't yet marked as read
@@ -120,23 +110,30 @@ void itemview_formaction::prepare() {
 			unread_item_count--;
 		set_head(item->title(), feedtitle, unread_item_count, feed->items().size());
 
-		unsigned int textwidth = v->get_cfg()->get_configvalue_as_int("text-width");
-		if (textwidth > 0) {
-			render_width = textwidth;
-		}
-
+		std::vector<std::pair<LineType, std::string>> lines;
 		if (show_source) {
-			render_source(lines, utils::quote_for_stfl(item->description()), render_width);
+			render_source(lines, utils::quote_for_stfl(item->description()));
 		} else {
 			std::string baseurl = item->get_base() != "" ? item->get_base() : item->feedurl();
-			lines = render_html(item->description(), links, baseurl, render_width);
+			lines = render_html(item->description(), links, baseurl);
 		}
 
-		listfmt.add_lines(lines, view_width);
+		textfmt.add_lines(lines);
 
-		num_lines = listfmt.get_lines_count();
+		unsigned int textwidth = v->get_cfg()->get_configvalue_as_int("text-width");
+		if (textwidth == 0) {
+			std::string widthstr = f->get("article:w");
+			if (widthstr.length() > 0) {
+				textwidth = utils::to_u(widthstr);
+				if (textwidth - 5 > 0) {
+					textwidth -= 5;
+				}
+			}
+		}
+		auto formatted_text = textfmt.format_text_to_list(rxman, "article", textwidth);
+		num_lines = formatted_text.size();
 
-		f->modify("article","replace_inner",listfmt.format_list(rxman, "article"));
+		f->modify("article", "replace_inner", formatted_text);
 		f->set("articleoffset","0");
 
 		if (in_search) {
@@ -428,37 +425,25 @@ void itemview_formaction::set_head(const std::string& s, const std::string& feed
 	f->set("head",fmt.do_format(v->get_cfg()->get_configvalue("itemview-title-format"), width));
 }
 
-void itemview_formaction::render_source(std::vector<std::string>& lines, std::string desc, unsigned int width) {
+void itemview_formaction::render_source(
+		std::vector<std::pair<LineType, std::string>>& lines,
+		std::string source)
+{
 	/*
-	 * this function is called instead of htmlrenderer::render() when the
+	 * This function is called instead of htmlrenderer::render() when the
 	 * user requests to have the source displayed instead of seeing the
 	 * rendered HTML.
 	 */
 	std::string line;
 	do {
-		std::string::size_type pos = desc.find_first_of("\r\n");
-		line = desc.substr(0,pos);
+		std::string::size_type pos = source.find_first_of("\r\n");
+		line = source.substr(0, pos);
 		if (pos == std::string::npos)
-			desc.erase();
+			source.erase();
 		else
-			desc.erase(0,pos+1);
-		while (line.length() > width) {
-			int i = width;
-			while (i > 0 && line[i] != ' ' && line[i] != '<')
-				--i;
-			if (0 == i) {
-				i = width;
-			}
-			std::string subline = line.substr(0, i);
-			line.erase(0, i);
-			pos = subline.find_first_not_of(" ");
-			subline.erase(0,pos);
-			lines.push_back(subline);
-		}
-		pos = line.find_first_not_of(" ");
-		line.erase(0,pos);
-		lines.push_back(line);
-	} while (desc.length() > 0);
+			source.erase(0, pos+1);
+		lines.push_back(std::make_pair(nonwrappable, line));
+	} while (source.length() > 0);
 }
 
 void itemview_formaction::handle_cmdline(const std::string& cmd) {
@@ -530,12 +515,17 @@ void itemview_formaction::finished_qna(operation op) {
 	}
 }
 
-std::vector<std::string> itemview_formaction::render_html(const std::string& source, std::vector<linkpair>& thelinks, const std::string& url, unsigned int render_width) {
-	std::vector<std::string> lines;
+std::vector<std::pair<LineType, std::string>>
+	itemview_formaction::render_html(
+		const std::string& source,
+		std::vector<linkpair>& thelinks,
+		const std::string& url)
+{
+	std::vector<std::pair<LineType, std::string>> result;
 	std::string renderer = v->get_cfg()->get_configvalue("html-renderer");
 	if (renderer == "internal") {
-		htmlrenderer rnd(render_width);
-		rnd.render(source, lines, thelinks, url);
+		htmlrenderer rnd;
+		rnd.render(source, result, thelinks, url);
 	} else {
 		char * argv[4];
 		argv[0] = const_cast<char *>("/bin/sh");
@@ -550,11 +540,11 @@ std::vector<std::string> itemview_formaction::render_html(const std::string& sou
 		std::string line;
 		getline(is, line);
 		while (!is.eof()) {
-			lines.push_back(utils::quote_for_stfl(line));
+			result.push_back(std::make_pair(newsbeuter::nonwrappable, utils::quote_for_stfl(line)));
 			getline(is, line);
 		}
 	}
-	return lines;
+	return result;
 }
 
 void itemview_formaction::set_regexmanager(regexmanager * r) {

src/textformatter.cpp
@@ -1,0 +1,158 @@
+#include <textformatter.h>
+#include <utils.h>
+#include <htmlrenderer.h>
+#include <stflpp.h>
+#include <assert.h>
+#include <limits.h>
+
+namespace newsbeuter {
+
+textformatter::textformatter() { }
+
+textformatter::~textformatter() { }
+
+void textformatter::add_line(LineType type, std::string line) {
+	LOG(LOG_DEBUG,
+		"textformatter::add_line: `%s' (line type %i)",
+		line.c_str(),
+		type);
+
+	auto clean_line =
+		utils::wstr2str(
+			utils::clean_nonprintable_characters(utils::str2wstr(line)));
+	lines.push_back(std::make_pair(type, clean_line));
+}
+
+void textformatter::add_lines(
+		const std::vector<std::pair<LineType, std::string>> lines)
+{
+	for (auto line : lines) {
+		add_line(line.first,
+			utils::replace_all(line.second, "\t", "        "));
+	}
+}
+
+std::vector<std::string> wrap_line(
+		const std::string& line,
+		const size_t width)
+{
+	std::vector<std::string> result;
+	std::vector<std::string> words = utils::tokenize_spaced(line);
+	std::string curline = "";
+
+	for (auto word : words){
+		size_t word_length = utils::strwidth_stfl(word);
+		size_t curline_length = utils::strwidth_stfl(curline);
+
+		// For words wider than the available width we have no other choice but
+		// to force splits at width limit
+		while (word_length > width) {
+			size_t space_left = width - curline_length;
+			curline.append(word.substr(0, space_left));
+			word.erase(0, space_left);
+			result.push_back(curline);
+			curline = "";
+
+			word_length = utils::strwidth_stfl(word);
+			curline_length = utils::strwidth_stfl(curline);
+		}
+
+		if ((curline_length + word_length) > width) {
+			result.push_back(curline);
+			if (word == " ") {
+				curline = "";
+			} else {
+				curline = word;
+			}
+		} else {
+			curline.append(word);
+		}
+	}
+
+	if (curline.length() > 0) {
+		result.push_back(curline);
+	}
+
+	return result;
+}
+
+std::string format_text_plain_helper(
+		const std::vector<std::pair<LineType, std::string>>& lines,
+		regexmanager * rxman,
+		const std::string& location,
+		const size_t width)
+{
+	LOG(LOG_DEBUG,
+		"textformatter::format_text_plain: rxman = %p, location = `%s', width = %zu, %u lines",
+		rxman, location.c_str(), width, lines.size());
+
+	std::string format_cache;
+	auto store_line =
+		[&format_cache]
+		(std::string line) {
+			format_cache.append(line + "\n");
+
+			LOG(LOG_DEBUG,
+				"textformatter::format_text_plain: stored `%s'",
+				line.c_str());
+		};
+	for (auto line : lines) {
+		auto type = line.first;
+		auto text = line.second;
+
+		LOG(LOG_DEBUG,
+			"textformatter::format_text_plain: got line `%s' type %u",
+			text.c_str(), type);
+
+		if (rxman && type != hr) {
+			rxman->quote_and_highlight(text, location);
+		}
+
+		switch(type) {
+			case wrappable:
+				for(auto line : wrap_line(text, width)) {
+					store_line(line);
+				}
+				break;
+
+			case nonwrappable:
+				store_line(text);
+				break;
+
+			case hr:
+				store_line(htmlrenderer::render_hr(width));
+				break;
+		}
+	}
+
+	return format_cache;
+}
+
+std::string textformatter::format_text_to_list(
+		regexmanager * rxman,
+		const std::string& location,
+		const size_t width)
+{
+	auto formatted = format_text_plain_helper(lines, rxman, location, width);
+
+	auto format_cache = std::string("{list");
+	for (auto line : utils::tokenize_nl(formatted)) {
+		if (line != "\n") {
+			utils::trim_end(line);
+			format_cache.append(
+					utils::strprintf(
+						"{listitem text:%s}",
+						stfl::quote(line).c_str()));
+		}
+	}
+
+	format_cache.append(1, '}');
+
+	return format_cache;
+}
+
+std::string textformatter::format_text_plain(const size_t width) {
+	return format_text_plain_helper(lines, NULL, std::string(), width);
+}
+
+}

test/textformatter.cpp
@@ -1,0 +1,93 @@
+#include "catch.hpp"
+
+#include <textformatter.h>
+
+using namespace newsbeuter;
+
+TEST_CASE("textformatter: lines marked as `wrappable` are wrapped to fit width") {
+	textformatter fmt;
+
+	fmt.add_lines(
+		{
+			std::make_pair(wrappable, "this one is going to be wrapped"),
+			std::make_pair(nonwrappable, "this one is going to be preserved")
+		});
+
+	SECTION("formatting to plain text") {
+		const std::string expected =
+			"this one \n"
+			"is going \n"
+			"to be \n"
+			"wrapped\n"
+			"this one is going to be preserved\n";
+		REQUIRE(fmt.format_text_plain(10) == expected);
+	}
+
+	SECTION("formatting to list") {
+		const std::string expected =
+			"{list"
+				"{listitem text:\"this one \"}"
+				"{listitem text:\"is going \"}"
+				"{listitem text:\"to be \"}"
+				"{listitem text:\"wrapped\"}"
+				"{listitem text:\"this one is going to be preserved\"}"
+			"}";
+		REQUIRE(fmt.format_text_to_list(NULL, "", 10) == expected);
+	}
+}
+
+TEST_CASE("textformatter: regex manager is used by format_text_to_list if one is passed") {
+	textformatter fmt;
+
+	fmt.add_line(wrappable, "Highlight me please!");
+
+	regexmanager rxmgr;
+	// the choice of green text on red background does not reflect my personal
+	// taste (or lack thereof) :)
+	rxmgr.handle_action("highlight", {"article", "please", "green", "default"});
+
+	const std::string expected =
+		"{list"
+			"{listitem text:\"Highlight me <0>please</>!\"}"
+		"}";
+
+	REQUIRE(fmt.format_text_to_list(&rxmgr, "article", 100) == expected);
+}
+
+TEST_CASE("textformatter: <hr> is rendered properly") {
+	textformatter fmt;
+
+	fmt.add_line(hr, "");
+
+	SECTION("width = 10") {
+		const std::string expected =
+			"\n"
+			" -------- "
+			"\n"
+			"\n";
+		REQUIRE(fmt.format_text_plain(10) == expected);
+	}
+}
+
+TEST_CASE("textformatter: wrappable sequences longer then format width are forced-wrapped") {
+	textformatter fmt;
+	fmt.add_line(wrappable, "0123456789");
+	fmt.add_line(nonwrappable, "0123456789");
+
+	const std::string expected =
+		"01234\n"
+		"56789\n"
+		"0123456789\n";
+	REQUIRE(fmt.format_text_plain(5) == expected);
+}
+
+TEST_CASE("textformatter: when wrapping, spaces at the beginning of lines are dropped") {
+	textformatter fmt;
+	fmt.add_line(wrappable, "just a test");
+
+	const std::string expected =
+		"just\n"
+		"a \n"
+		"test\n";
+	REQUIRE(fmt.format_text_plain(4) == expected);
+}

doc/configcommands.dsv
@@ -91,7 +91,7 @@ show-read-articles|[yes/no]|yes|If yes, then all articles of a feed are listed i
 show-read-feeds|[yes/no]|yes|If yes, then all feeds, including those without unread articles, are listed. If no, then only feeds with one or more unread articles are list.|show-read-feeds no
 suppress-first-reload|[yes/no]|no|If yes, then the first automatic reload will be suppressed if auto-reload is set to yes.|suppress-first-reload yes
 swap-title-and-hints|[yes/no]|no|If yes, then the title at the top of screen and keymap hints at the bottom of screen will be swapped.|swap-title-and-hints yes
-text-width|<number>|0|If set to a number greater than 0, then all HTML will be rendered to this maximum line length. If set to 0, the terminal width will be used.|text-width 72
+text-width|<number>|0|If set to a number greater than 0, then all HTML will be rendered to this maximum line length. If set to 0, the terminal width will be used. Does not apply when using external renderer or viewing the source.|text-width 72
 ttrss-flag-publish|<character>|""|If this is set and Tiny Tiny RSS support is used, then all articles that are flagged with the specified flag are being marked as "published" in Tiny Tiny RSS.|ttrss-flag-publish "b"
 ttrss-flag-star|<character>|""|If this is set and Tiny Tiny RSS support is used, then all articles that are flagged with the specified flag are being "starred" in Tiny Tiny RSS.|ttrss-flag-star "a"
 ttrss-login|<username>|""|Sets the username for use with Tiny Tiny RSS.|ttrss-login "admin"

include/htmlrenderer.h
@@ -1,6 +1,7 @@
 #ifndef NEWSBEUTER_HTMLRENDERER__H
 #define NEWSBEUTER_HTMLRENDERER__H
 
+#include <textformatter.h>
 #include <vector>
 #include <string>
 #include <istream>
@@ -21,9 +22,16 @@ typedef std::pair<std::string,link_type> linkpair;
 
 class htmlrenderer {
 	public:
-		htmlrenderer(unsigned int width = 80, bool raw = false);
-		void render(const std::string&, std::vector<std::string>& lines,  std::vector<linkpair>& links, const std::string& url);
-		void render(std::istream &, std::vector<std::string>& lines, std::vector<linkpair>& links, const std::string& url);
+		htmlrenderer(bool raw = false);
+		void render(const std::string& source,
+				std::vector<std::pair<LineType, std::string>>& lines,
+				std::vector<linkpair>& links,
+				const std::string& url);
+		void render(std::istream & input,
+				std::vector<std::pair<LineType, std::string>>& lines,
+				std::vector<linkpair>& links,
+				const std::string& url);
+		static std::string render_hr(const unsigned int width);
 		// only public for unit testing purposes:
 		std::string format_ol_count(unsigned int count, char type);
 
@@ -59,7 +67,6 @@ class htmlrenderer {
 		};
 
 	private:
-		unsigned int w;
 		void prepare_new_line(std::string& line, int indent_level);
 		bool line_is_nonempty(const std::string& line);
 		unsigned int add_link(std::vector<linkpair>& links, const std::string& link, link_type type);
@@ -67,9 +74,21 @@ class htmlrenderer {
 		std::string absolute_url(const std::string& url, const std::string& link);
 		std::string type2str(link_type type);
 		std::map<std::string, htmltag> tags;
-		void render_table(const Table& table, std::vector<std::string>& lines);
-		void add_nonempty_line(const std::string& curline, std::vector<Table>& tables, std::vector<std::string>& lines);
-		void add_line(const std::string& curline, std::vector<Table>& tables, std::vector<std::string>& lines);
+		void render_table(
+				const Table& table,
+				std::vector<std::pair<LineType, std::string>>& lines);
+		void add_nonempty_line(
+				const std::string& curline,
+				std::vector<Table>& tables,
+				std::vector<std::pair<LineType, std::string>>& lines);
+		void add_line(
+				const std::string& curline,
+				std::vector<Table>& tables,
+				std::vector<std::pair<LineType, std::string>>& lines);
+		void add_line_verbatim(
+				const std::string& line,
+				std::vector<std::pair<LineType, std::string>>& lines);
+		void add_hr(std::vector<std::pair<LineType, std::string>>& lines);
 		std::string get_char_numbering(unsigned int count);
 		std::string get_roman_numbering(unsigned int count);
 		bool raw_;

include/itemview_formaction.h
@@ -3,6 +3,7 @@
 
 #include <formaction.h>
 #include <htmlrenderer.h>
+#include <textformatter.h>
 #include <regexmanager.h>
 #include <rss.h>
 
@@ -33,7 +34,11 @@ class itemview_formaction : public formaction {
 
 		virtual void finished_qna(operation op);
 
-		std::vector<std::string> render_html(const std::string& source, std::vector<linkpair>& links, const std::string& feedurl, unsigned int render_width);
+		std::vector<std::pair<LineType, std::string>>
+			render_html(
+				const std::string& source,
+				std::vector<linkpair>& thelinks,
+				const std::string& url);
 
 		void set_regexmanager(regexmanager * r);
 
@@ -44,7 +49,9 @@ class itemview_formaction : public formaction {
 		void set_head(const std::string& s, const std::string& feedtitle, unsigned int unread, unsigned int total);
 		void highlight_text(const std::string& searchphrase);
 
-		void render_source(std::vector<std::string>& lines, std::string desc, unsigned int width);
+		void render_source(
+				std::vector<std::pair<LineType, std::string>>& lines,
+				std::string source);
 
 		void do_search();
 

mk/mk.deps
@@ -99,3 +99,5 @@ src/rss_parser.o: include/rss_parser.h include/configcontainer.h include/cache.h
 src/ttrss_api.o: include/ttrss_api.h
 
 src/newsblur_api.o: include/newsblur_api.h
+
+src/textformatter.o: include/textformatter.h include/utils.h include/htmlrenderer.h include/stflpp.h

commit 4be8103b47ce107fbf2d50555b8f9cd885c5f33b

Fix a bug I introduced in 885760cf70c35fb632d2c872
One of the fields of `itemview_formaction` (namely, `num_lines`) wasn't
updated properly: it's supposed to contain the number of lines of text
displayed in the view, but I stored the *length* of the text there. As
a consequence, the reading progress indicator always said "Top" instead
of being updated as the user scrolls through the article.

include/textformatter.h
@@ -36,7 +36,7 @@ class textformatter {
 		void add_line(LineType type, std::string line);
 		void add_lines(
 				const std::vector<std::pair<LineType, std::string>> lines);
-		std::string format_text_to_list(
+		std::pair<std::string, std::size_t> format_text_to_list(
 				regexmanager * r = nullptr,
 				const std::string& location = "",
 				const size_t wrap_width = 80,

src/itemview_formaction.cpp
@@ -131,10 +131,10 @@ void itemview_formaction::prepare() {
 				textwidth -= 5;
 			}
 		}
-		auto formatted_text = textfmt.format_text_to_list(
-				rxman, "article", textwidth, window_width);
-		num_lines = formatted_text.size();
 
+		std::string formatted_text;
+		std::tie(formatted_text, num_lines) = textfmt.format_text_to_list(
+				rxman, "article", textwidth, window_width);
 		f->modify("article", "replace_inner", formatted_text);
 		f->set("articleoffset","0");
 

src/textformatter.cpp
@@ -77,7 +77,7 @@ std::vector<std::string> wrap_line(
 	return result;
 }
 
-std::string format_text_plain_helper(
+std::vector<std::string> format_text_plain_helper(
 		const std::vector<std::pair<LineType, std::string>>& lines,
 		regexmanager * rxman,
 		const std::string& location,
@@ -91,16 +91,18 @@ std::string format_text_plain_helper(
 		"wrap_width = %zu, total_width = %zu, %u lines",
 		rxman, location, wrap_width, total_width, lines.size());
 
-	std::string format_cache;
+	std::vector<std::string> format_cache;
+
 	auto store_line =
 		[&format_cache]
 		(std::string line) {
-			format_cache.append(line + "\n");
+			format_cache.push_back(line);
 
 			LOG(level::DEBUG,
 				"textformatter::format_text_plain: stored `%s'",
 				line);
 		};
+
 	for (auto line : lines) {
 		auto type = line.first;
 		auto text = line.second;
@@ -143,7 +145,8 @@ std::string format_text_plain_helper(
 	return format_cache;
 }
 
-std::string textformatter::format_text_to_list(
+std::pair<std::string, std::size_t>
+textformatter::format_text_to_list(
 		regexmanager * rxman,
 		const std::string& location,
 		const size_t wrap_width,
@@ -153,8 +156,8 @@ std::string textformatter::format_text_to_list(
 			lines, rxman, location, wrap_width, total_width);
 
 	auto format_cache = std::string("{list");
-	for (auto line : utils::tokenize_nl(formatted)) {
-		if (line != "\n") {
+	for (auto line : formatted) {
+		if (line != "") {
 			utils::trim_end(line);
 			format_cache.append(
 					strprintf::fmt(
@@ -162,14 +165,24 @@ std::string textformatter::format_text_to_list(
 						stfl::quote(line)));
 		}
 	}
-
 	format_cache.append(1, '}');
 
-	return format_cache;
+	auto line_count = formatted.size();
+
+	return { format_cache, line_count };
 }
 
-std::string textformatter::format_text_plain(const size_t width, const size_t total_width) {
-	return format_text_plain_helper(lines, nullptr, std::string(), width, total_width);
+std::string textformatter::format_text_plain(
+		const size_t width, const size_t total_width)
+{
+	std::string result;
+	auto formatted = format_text_plain_helper(
+			lines, nullptr, "", width, total_width);
+	for (const auto& line : formatted) {
+		result += line + "\n";
+	}
+
+	return result;
 }
 
 }

test/textformatter.cpp
@@ -28,7 +28,7 @@ TEST_CASE("lines marked as `wrappable` are wrapped to fit width",
 	}
 
 	SECTION("formatting to list") {
-		const std::string expected =
+		const std::string expected_text =
 			"{list"
 				"{listitem text:\"this one \"}"
 				"{listitem text:\"is going \"}"
@@ -38,7 +38,12 @@ TEST_CASE("lines marked as `wrappable` are wrapped to fit width",
 				"{listitem text:\"window border\"}"
 				"{listitem text:\"this one is going to be preserved even though it's much longer\"}"
 			"}";
-		REQUIRE(fmt.format_text_to_list(nullptr, "", 10, 40) == expected);
+		const std::size_t expected_count = 7;
+
+		const auto result = fmt.format_text_to_list(nullptr, "", 10, 40);
+
+		REQUIRE(result.first == expected_text);
+		REQUIRE(result.second == expected_count);
 	}
 }
 
@@ -53,12 +58,16 @@ TEST_CASE("regex manager is used by format_text_to_list if one is passed",
 	// taste (or lack thereof) :)
 	rxmgr.handle_action("highlight", {"article", "please", "green", "default"});
 
-	const std::string expected =
+	const std::string expected_text =
 		"{list"
 			"{listitem text:\"Highlight me <0>please</>!\"}"
 		"}";
+	const std::size_t expected_count = 1;
 
-	REQUIRE(fmt.format_text_to_list(&rxmgr, "article", 100) == expected);
+	const auto result = fmt.format_text_to_list(&rxmgr, "article", 100);
+
+	REQUIRE(result.first == expected_text);
+	REQUIRE(result.second == expected_count);
 }
 
 TEST_CASE("<hr> is rendered properly", "[textformatter]") {
@@ -129,20 +138,30 @@ TEST_CASE("softwrappable lines are wrapped by format_text_to_list if "
 	const std::string location = "";
 
 	SECTION("total_width == 4") {
-		const std::string expected =
+		const std::string expected_text =
 			"{list"
 				"{listitem text:\"just\"}"
 				"{listitem text:\"a \"}"
 				"{listitem text:\"test\"}"
 			"}";
-		REQUIRE(fmt.format_text_to_list(rxman, location, wrap_width, 4) == expected);
+		const std::size_t expected_count = 3;
+
+		const auto result = fmt.format_text_to_list(rxman, location, wrap_width, 4);
+
+		REQUIRE(result.first == expected_text);
+		REQUIRE(result.second == expected_count);
 	}
 
 	SECTION("total_width == 0") {
-		const std::string expected =
+		const std::string expected_text =
 			"{list"
 				"{listitem text:\"just a test\"}"
 			"}";
-		REQUIRE(fmt.format_text_to_list(rxman, location, wrap_width, 0) == expected);
+		const std::size_t expected_count = 1;
+
+		const auto result = fmt.format_text_to_list(rxman, location, wrap_width, 0);
+
+		REQUIRE(result.first == expected_text);
+		REQUIRE(result.second == expected_count);
 	}
 }