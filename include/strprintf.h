c6bf09889628ec4c7b65fc94471532caf9fd6865
Alexander Batischev <eual.jp@gmail.com>
2016-11-20 23:14:49 +0300

    Silence Clang's warning about snprintf

--- include/strprintf.h
+++ include/strprintf.h
@@ -14,13 +14,22 @@ class strprintf {
 		static std::string fmt(const std::string& format) {
 			char buffer[1024];
 			std::string result;
+			// Empty string is a dummy value that we pass in order to silence
+			// Clang's warning about format not being a literal.
+			//
+			// The thing is, at this point we know *for sure* that the format
+			// either contains no formats at all, or only escaped percent signs
+			// (which don't require any additional arguments to snprintf). It's
+			// just the way fmt recurses. The only reason we're calling
+			// snprintf at all is to process these escaped percent signs, if
+			// any. So we don't need additional parameters.
 			unsigned int len = 1 + snprintf(
-					buffer, sizeof(buffer), format.c_str());
+					buffer, sizeof(buffer), format.c_str(), "");
 			if (len <= sizeof(buffer)) {
 				result = buffer;
 			} else {
 				std::unique_ptr<char> buf(new char[len]);
-				snprintf(buf.get(), len, format.c_str());
+				snprintf(buf.get(), len, format.c_str(), "");
 				result = *buf;
 			}
 			return result;
