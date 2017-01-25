0e108bfe5c59a185f479f0a1a9b97198cc4bfc32
Alexander Batischev <eual.jp@gmail.com>
2017-01-25 20:55:21 +0300

    Update copyright notices

--- src/controller.cpp
+++ src/controller.cpp
@@ -971,7 +971,10 @@ void controller::start_reload_all_thread(std::vector<int> * indexes) {
 void controller::version_information(const char * argv0, unsigned int level) {
 	if (level<=1) {
 		std::cout << PROGRAM_NAME << " " << PROGRAM_VERSION << " - " << PROGRAM_URL << std::endl;
-		std::cout << "Copyright (C) 2006-2015 Andreas Krennmair" << std::endl << std::endl;
+		std::cout << "Copyright (C) 2006-2015 Andreas Krennmair" << std::endl;
+		std::cout << "Copyright (C) 2015-2017 Alexander Batischev" << std::endl;
+		std::cout << "Copyright (C) 2006-2017 Newsbeuter contributors" << std::endl;
+		std::cout << std::endl;
 
 		std::cout << _("newsbeuter is free software and licensed under the MIT/X Consortium License.") << std::endl;
 		std::cout << strprintf::fmt(_("Type `%s -vv' for more information."), argv0) << std::endl << std::endl;
