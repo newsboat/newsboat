e69bc0e4b52ef1f695cecc9a52787b4b2bb08f92
Alexander Batischev <eual.jp@gmail.com>
2017-12-28 19:41:16 +0300

    Prune unrelated stuff from coverage analysis

--- test/generate_coverage_report.sh
+++ test/generate_coverage_report.sh
@@ -14,7 +14,22 @@ lcov --capture --initial --base-directory . --directory . --output-file $APPBASE
 lcov --capture --base-directory . --directory . --output-file $APPTEST_INFO
 lcov --base-directory . --directory . --output-file $APPTOTAL_INFO \
     --add-tracefile $APPBASE_INFO --add-tracefile $APPTEST_INFO
+
+# Removing info about shared libraries
 lcov --remove $APPTOTAL_INFO '/usr/*' --output-file $APPTOTAL_INFO
+# Removing info about shared libraries (on NixOS)
+lcov --remove $APPTOTAL_INFO '/nix/store/*' --output-file $APPTOTAL_INFO
+# Removing info about compiler internals
+lcov --remove $APPTOTAL_INFO '?.?.?/*' --output-file $APPTOTAL_INFO
+lcov --remove $APPTOTAL_INFO '?.?.??/*' --output-file $APPTOTAL_INFO
+lcov --remove $APPTOTAL_INFO '?.??.?/*' --output-file $APPTOTAL_INFO
+lcov --remove $APPTOTAL_INFO '?.??.??/*' --output-file $APPTOTAL_INFO
+# Removing info about Newsboat's tests
 lcov --remove $APPTOTAL_INFO 'newsboat/test/*' --output-file $APPTOTAL_INFO
+lcov --remove $APPTOTAL_INFO '*/newsboat/test/*' --output-file $APPTOTAL_INFO
+# Removing info about Newsboat's docs
+lcov --remove $APPTOTAL_INFO 'newsboat/doc/*' --output-file $APPTOTAL_INFO
+lcov --remove $APPTOTAL_INFO '*/newsboat/doc/*' --output-file $APPTOTAL_INFO
+
 genhtml -o html $APPTOTAL_INFO
 echo "The coverage report can be found at file://`pwd`/html/index.html"
