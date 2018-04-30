e058b39199a5cda46de9cc1f7f60776b6982d30b
Alexander Batischev <eual.jp@gmail.com>
2018-04-30 13:33:37 +0300

    Check return code of mkdir_parents in pb_controller
    Bug found by Coverity Scan.

--- src/pb_controller.cpp
+++ src/pb_controller.cpp
@@ -96,7 +96,14 @@ bool pb_controller::setup_dirs_xdg(const char *env_home) {
 	config_dir = xdg_config_dir;
 
 	// create data directory if it doesn't exist
-	utils::mkdir_parents(xdg_data_dir, 0700);
+	if (0 != utils::mkdir_parents(xdg_data_dir, 0700)) {
+		LOG(level::CRITICAL,
+				"Couldn't create `%s': (%i) %s",
+				xdg_data_dir,
+				errno,
+				strerror(errno));
+		::exit(EXIT_FAILURE);
+	}
 
 	/* in config */
 	url_file = config_dir + std::string(NEWSBEUTER_PATH_SEP) + url_file;
