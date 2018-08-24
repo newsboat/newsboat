2760d33b7f3dded4dea6a94f915d5aecb2043a5d
kmws <kamil.wsol@gmail.com>
2018-08-24 19:45:41 +0200

    Add missing braces

--- src/controller.cpp
+++ src/controller.cpp
@@ -1055,8 +1055,9 @@ void controller::write_item(std::shared_ptr<rss_item> item,
 {
 	std::fstream f;
 	f.open(filename.c_str(), std::fstream::out);
-	if (!f.is_open())
+	if (!f.is_open()) {
 		throw exception(errno);
+	}
 
 	write_item(item, f);
 }
