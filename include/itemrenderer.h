85fd1a7862017099dcd6801dcd15f588e9bfdfa9
Alexander Batischev <eual.jp@gmail.com>
2018-10-13 12:31:14 +0300

    Add API docs for item_renderer functions

--- include/itemrenderer.h
+++ include/itemrenderer.h
@@ -13,13 +13,26 @@ namespace newsboat {
 class ConfigContainer;
 class RssItem;
 
+/// \brief Creates different textual representations of an RssItem.
 namespace item_renderer {
+	/// \brief Returns feed's title of the item, or some replacement value if
+	/// the title isn't available.
 	std::string get_feedtitle(std::shared_ptr<RssItem> item);
 
+	/// \brief Returns plain-text representation of the RssItem.
+	///
+	/// Markup is mostly stripped. Links are numbered and tagged. The text is
+	/// wrapped at the width specified by the `text-width` setting.
 	std::string to_plain_text(
 			ConfigContainer& cfg,
 			std::shared_ptr<RssItem> item);
 
+	/// \brief Returns RssItem as STFL list.
+	///
+	/// `html-renderer` settings controlls what tool is used to render HTML. \a
+	/// text_width dictates where text is wrapped. \a window_width dictates
+	/// where URLs are wrapped. \a rxman rules for \a location are used to
+	/// highlight the resulting text.
 	std::pair<std::string, size_t> to_stfl_list(
 			ConfigContainer& cfg,
 			std::shared_ptr<RssItem> item,
@@ -28,6 +41,11 @@ namespace item_renderer {
 			RegexManager* rxman,
 			const std::string& location);
 
+	/// \brief Returns RssItem's text source as STFL list.
+	///
+	/// \a text_width dictates where text is wrapped. \a window_width dictates
+	/// where URLs are wrapped. \a rxman rules for \a location are used to
+	/// highlight the resulting text.
 	std::pair<std::string, size_t> source_to_stfl_list(
 			std::shared_ptr<RssItem> item,
 			unsigned int text_width,
