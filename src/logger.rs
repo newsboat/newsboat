fedd972ebd77766a60094557f39676f0c2a47cd5
Alexander Batischev <eual.jp@gmail.com>
2018-10-18 23:23:38 +0300

    Logger::log writes a line to the file

--- src/logger.rs
+++ src/logger.rs
@@ -1,4 +1,5 @@
 use std::fs::{File, OpenOptions};
+use std::io::{self, Write};
 
 pub struct Logger {
     logfile: Option<File>,
@@ -22,6 +23,14 @@ impl Logger {
             Err(error) => eprintln!("Couldn't open `{}' as a logfile: {}", filename, error),
         }
     }
+
+    pub fn log(&mut self, message: &str) -> io::Result<()> {
+        if let Some(ref mut logfile) = self.logfile {
+            logfile.write_all(message.as_bytes())?;
+        }
+
+        Ok(())
+    }
 }
 
 #[cfg(test)]
@@ -30,24 +39,49 @@ mod tests {
 
     use super::*;
     
-    use std::io;
     use self::tempfile::TempDir;
+    use std::io::{self, Read};
+    use std::path;
+
+    fn setup_logger() -> io::Result<(TempDir, path::PathBuf, Logger)> {
+        let tmp = TempDir::new()?;
+        let logfile = {
+            let mut logfile = tmp.path().to_owned();
+            logfile.push("example.log");
+            logfile
+        };
+        assert!(!logfile.exists());
+
+        let mut logger = Logger::new();
+        logger.set_logfile(logfile.to_str().unwrap());
+
+        Ok((tmp, logfile, logger))
+    }
 
     #[test]
     fn t_set_logfile() -> io::Result<()> {
-        let tmp = TempDir::new()?;
-        let filename = {
-            let mut filename = tmp.path().to_owned();
-            filename.push("example.log");
-            filename
-        };
+        let (_tmp, logfile, _logger) = setup_logger()?;
 
-        assert!(!filename.exists());
+        assert!(logfile.exists());
 
-        let mut logger = Logger::new();
-        logger.set_logfile(filename.to_str().unwrap());
+        Ok(())
+    }
 
-        assert!(filename.exists());
+    #[test]
+    fn t_log_writes_message_to_the_file() -> io::Result<()> {
+        let (_tmp, logfile, mut logger) = setup_logger()?;
+
+        let message = "Hello, world!";
+        logger.log(message)?;
+
+        // Dropping logger to force it to flush the log and close the file
+        drop(logger);
+
+        let mut file = File::open(logfile)?;
+        let mut contents = String::new();
+        file.read_to_string(&mut contents)?;
+
+        assert_eq!(contents, message);
 
         Ok(())
     }
