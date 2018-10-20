e0d1d31c5a0e8c9a5e55d5c0c1a129cf7a2db116
Alexander Batischev <eual.jp@gmail.com>
2018-10-20 23:02:15 +0300

    Timestamp each log message

--- src/logger.rs
+++ src/logger.rs
@@ -1,3 +1,6 @@
+extern crate chrono;
+
+use self::chrono::offset::Local;
 use std::fs::{File, OpenOptions};
 use std::io::{self, Write};
 
@@ -26,8 +29,10 @@ impl Logger {
 
     pub fn log(&mut self, message: &str) -> io::Result<()> {
         if let Some(ref mut logfile) = self.logfile {
-            logfile.write_all(message.as_bytes())?;
-            logfile.write_all("\n".as_bytes())?;
+            let timestamp = Local::now().format("%Y-%m-%d %H:%M:%S");
+            let line = format!("[{}] {}\n", timestamp, message);
+
+            logfile.write_all(line.as_bytes())?;
         }
 
         Ok(())
@@ -41,6 +46,7 @@ mod tests {
     use super::*;
     
     use self::tempfile::TempDir;
+    use self::chrono::{TimeZone, Duration};
     use std::io::{self, BufReader, BufRead};
     use std::path;
 
@@ -79,9 +85,12 @@ mod tests {
             messages.push("Time to wrap up, see ya!");
             messages
         };
+
+        let start_time = Local::now();
         for msg in &messages {
             logger.log(msg)?;
         }
+        let finish_time = Local::now();
 
         // Dropping logger to force it to flush the log and close the file
         drop(logger);
@@ -90,7 +99,35 @@ mod tests {
         let reader = BufReader::new(file);
         for (line, expected) in reader.lines().zip(messages) {
             match line {
-                Ok(line) => assert_eq!(line, expected),
+                Ok(line) => {
+                    assert!(line.starts_with("["));
+
+                    let timestamp_end = line.find(']').expect("Failed to find the end of the timestamp");
+                    // "]" is an ASCII character, so it occupies one byte in UTF-8. As a result, we
+                    // can simply add one to get the next character's byte offset. And since the
+                    // next char should be a space, which is ASCII as well, we can just add 2.
+                    //
+                    // If timestamp is followed by something non-ASCII, indexing into &str will
+                    // panic and fail the test, which is good.
+                    assert_eq!(&line[timestamp_end+1..timestamp_end+2], " ");
+
+                    // Timestamp starts with "[", we skip it by starting at 1 rather than 0.
+                    let timestamp_str = &line[1..timestamp_end];
+                    let timestamp =
+                        Local::now()
+                        .timezone()
+                        .datetime_from_str(timestamp_str, "%Y-%m-%d %H:%M:%S")
+                        .expect("Failed to parse the timestamp from the log file");
+                    // `start_time` and `end_time` may have millisecond precision or better,
+                    // whereas `timestamp` is limited to seconds. Therefore, we account for
+                    // a situation where `start_time` is slightly bigger than `timestamp`.
+                    assert!(timestamp - start_time > Duration::seconds(-1));
+                    assert!(finish_time >= timestamp);
+
+                    // Message starts after "] " that follows the timestamp, hence +2.
+                    let message_str = &line[timestamp_end+2..];
+                    assert_eq!(message_str, expected);
+                }
                 Err(e) => return Err(e),
             }
         }
