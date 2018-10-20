d43a204f7d897e1d3cdbe6da41e1e04b19ce91e8
Alexander Batischev <eual.jp@gmail.com>
2018-10-21 01:47:41 +0300

    Ensure that each logging level has a unique tag
    Level::None is not tested because we shouldn't ever log anything with
    it. I'll enforce that in one of the forthcoming commits.

--- src/logger.rs
+++ src/logger.rs
@@ -5,6 +5,7 @@ use std::fmt;
 use std::fs::{File, OpenOptions};
 use std::io::{self, Write};
 
+#[derive(Clone, Copy)]
 pub enum Level {
     None,
     UserError,
@@ -90,6 +91,37 @@ mod tests {
         Ok((tmp, logfile, logger))
     }
 
+    fn parse_log_line(line: &str) -> Option<(&str, &str, &str)> {
+        if !line.starts_with("[") {
+            return None;
+        }
+
+        let timestamp_end = line.find(']').expect("Failed to find the end of the timestamp");
+        // Timestamp starts with "[", we skip it by starting at 1 rather than 0.
+        let timestamp = &line[1..timestamp_end];
+
+        // "]" is an ASCII character, so it occupies one byte in UTF-8. As a result, we
+        // can simply add one to get the next character's byte offset. And since the
+        // next char should be a space, which is ASCII as well, we can just add 2.
+        //
+        // If timestamp is followed by something non-ASCII, indexing into &str will
+        // panic and fail the test, which is good.
+        if &line[timestamp_end+1..timestamp_end+2] != " " {
+            return None;
+        }
+
+        let level_end =
+            line[timestamp_end+2..]
+            .find(':')
+            .expect("Failed to find the end of the loglevel");
+        let level = &line[timestamp_end+2..timestamp_end+2+level_end];
+
+        // Message starts after ": " that follows the level, hence +2.
+        let message = &line[timestamp_end+2+level_end+2..];
+
+        Some((timestamp, level, message))
+    }
+
     #[test]
     fn t_set_logfile() -> io::Result<()> {
         let (_tmp, logfile, _logger) = setup_logger()?;
@@ -103,13 +135,11 @@ mod tests {
     fn t_log_writes_message_to_the_file() -> io::Result<()> {
         let (_tmp, logfile, mut logger) = setup_logger()?;
 
-        let messages = {
-            let mut messages = vec![];
-            messages.push("Hello, world!");
-            messages.push("I'm doing fine, how are you?");
-            messages.push("Time to wrap up, see ya!");
-            messages
-        };
+        let messages = vec![
+            "Hello, world!",
+            "I'm doing fine, how are you?",
+            "Time to wrap up, see ya!",
+        ];
 
         let start_time = Local::now();
         for msg in &messages {
@@ -125,19 +155,10 @@ mod tests {
         for (line, expected) in reader.lines().zip(messages) {
             match line {
                 Ok(line) => {
-                    assert!(line.starts_with("["));
+                    let (timestamp_str, _level, message) =
+                        parse_log_line(&line)
+                        .expect("Failed to split the log line into parts");
 
-                    let timestamp_end = line.find(']').expect("Failed to find the end of the timestamp");
-                    // "]" is an ASCII character, so it occupies one byte in UTF-8. As a result, we
-                    // can simply add one to get the next character's byte offset. And since the
-                    // next char should be a space, which is ASCII as well, we can just add 2.
-                    //
-                    // If timestamp is followed by something non-ASCII, indexing into &str will
-                    // panic and fail the test, which is good.
-                    assert_eq!(&line[timestamp_end+1..timestamp_end+2], " ");
-
-                    // Timestamp starts with "[", we skip it by starting at 1 rather than 0.
-                    let timestamp_str = &line[1..timestamp_end];
                     let timestamp =
                         Local::now()
                         .timezone()
@@ -149,10 +170,47 @@ mod tests {
                     assert!(timestamp - start_time > Duration::seconds(-1));
                     assert!(finish_time >= timestamp);
 
-                    // Message starts after "] " that follows the timestamp, hence +2.
-                    let message_str = &line[timestamp_end+2..];
-                    let level = String::from("DEBUG: ");
-                    assert_eq!(message_str, level + expected);
+                    assert_eq!(message, expected);
+                }
+                Err(e) => return Err(e),
+            }
+        }
+
+        Ok(())
+    }
+
+    #[test]
+    fn t_different_loglevels_have_different_names() -> io::Result<()> {
+        let (_tmp, logfile, mut logger) = setup_logger()?;
+
+        let levels = vec![
+            (Level::UserError, "USERERROR"),
+            (Level::Critical, "CRITICAL"),
+            (Level::Error, "ERROR"),
+            (Level::Warn, "WARNING"),
+            (Level::Info, "INFO"),
+            (Level::Debug, "DEBUG"),
+        ];
+
+        let msg = "Some test message";
+
+        for (level, _level_str) in &levels {
+            logger.log(*level, msg)?;
+        }
+
+        // Dropping logger to force it to flush the log and close the file
+        drop(logger);
+
+        let file = File::open(logfile)?;
+        let reader = BufReader::new(file);
+        for (line, expected) in reader.lines().zip(levels.iter().map(|(_, l)| l)) {
+            match line {
+                Ok(line) => {
+                    let (_timestamp_str, level, _message) =
+                        parse_log_line(&line)
+                        .expect("Failed to split the log line into parts");
+
+                    assert_eq!(&level, expected);
                 }
                 Err(e) => return Err(e),
             }
