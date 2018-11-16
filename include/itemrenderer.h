newsboat/newsboat

commit 485082f43f49f1c30974621107b1fa197618bf67

Re-use item_renderer in ItemViewFormAction

include/itemrenderer.h
@@ -34,6 +34,21 @@ namespace item_renderer {
 		std::shared_ptr<RssItem> item,
 		std::vector<std::pair<LineType, std::string>>& lines,
 		std::vector<LinkPair>& /*links*/);
+
+	std::pair<std::string, size_t> to_stfl_list(
+			ConfigContainer& cfg,
+			std::shared_ptr<RssItem> item,
+			unsigned int text_width,
+			unsigned int window_width,
+			RegexManager* rxman,
+			const std::string& location);
+
+	std::pair<std::string, size_t> source_to_stfl_list(
+			std::shared_ptr<RssItem> item,
+			unsigned int text_width,
+			unsigned int window_width,
+			RegexManager* rxman,
+			const std::string& location);
 };
 
 } // namespace newsboat

src/itemrenderer.cpp
@@ -147,4 +147,67 @@ std::string item_renderer::to_plain_text(
 	return txtfmt.format_text_plain(width);
 }
 
+std::pair<std::string, size_t> item_renderer::to_stfl_list(
+		ConfigContainer& cfg,
+		std::shared_ptr<RssItem> item,
+		unsigned int text_width,
+		unsigned int window_width,
+		RegexManager* rxman,
+		const std::string& location)
+{
+	std::vector<std::pair<LineType, std::string>> lines;
+	std::vector<LinkPair> links;
+
+	prepare_header(item, lines, links);
+	const std::string baseurl = item_renderer::get_item_base_link(item);
+	const auto body = item->description();
+	render_html(cfg, body, lines, links, baseurl, false);
+
+	TextFormatter txtfmt;
+	txtfmt.add_lines(lines);
+
+	return txtfmt.format_text_to_list(rxman, location, text_width, window_width);
+}
+
+void render_source(
+	std::vector<std::pair<LineType, std::string>>& lines,
+	std::string source)
+{
+	/*
+	 * This function is called instead of HtmlRenderer::render() when the
+	 * user requests to have the source displayed instead of seeing the
+	 * rendered HTML.
+	 */
+	std::string line;
+	do {
+		std::string::size_type pos = source.find_first_of("\r\n");
+		line = source.substr(0, pos);
+		if (pos == std::string::npos) {
+			source.erase();
+		} else {
+			source.erase(0, pos + 1);
+		}
+		lines.push_back(std::make_pair(LineType::softwrappable, line));
+	} while (source.length() > 0);
+}
+
+std::pair<std::string, size_t> item_renderer::source_to_stfl_list(
+		std::shared_ptr<RssItem> item,
+		unsigned int text_width,
+		unsigned int window_width,
+		RegexManager* rxman,
+		const std::string& location)
+{
+	std::vector<std::pair<LineType, std::string>> lines;
+	std::vector<LinkPair> links;
+
+	prepare_header(item, lines, links);
+	render_source(lines, Utils::quote_for_stfl(item->description()));
+
+	TextFormatter txtfmt;
+	txtfmt.add_lines(lines);
+
+	return txtfmt.format_text_to_list(rxman, location, text_width, window_width);
+}
+
 }

src/itemviewformaction.cpp
@@ -91,36 +91,40 @@ void ItemViewFormAction::prepare()
 
 		update_head(item);
 
-		std::vector<std::pair<LineType, std::string>> lines;
-		item_renderer::prepare_header(item, lines, links);
-
-		if (show_source) {
-			render_source(lines, Utils::quote_for_stfl(item->description()));
-		} else {
-			const std::string baseurl = item_renderer::get_item_base_link(item);
-			const auto body = item->description();
-			item_renderer::render_html(*cfg, body, lines, links, baseurl, false);
-		}
-
 		std::string widthstr = f->get("article:w");
 		const unsigned int window_width = Utils::to_u(widthstr, 0);
 
-		unsigned int textwidth =
+		unsigned int text_width =
 			cfg->get_configvalue_as_int("text-width");
-		if (textwidth == 0 || textwidth > window_width) {
-			textwidth = window_width;
-			if (textwidth - 5 > 0) {
-				textwidth -= 5;
+		if (text_width == 0 || text_width > window_width) {
+			text_width = window_width;
+			if (text_width - 5 > 0) {
+				text_width -= 5;
 			}
 		}
 
-		TextFormatter textfmt;
-		textfmt.add_lines(lines);
-
 		std::string formatted_text;
-		std::tie(formatted_text, num_lines) =
-			textfmt.format_text_to_list(
-				rxman, "article", textwidth, window_width);
+		if (show_source) {
+			std::tie(formatted_text, num_lines) =
+				item_renderer::source_to_stfl_list(
+					item,
+					text_width,
+					window_width,
+					rxman,
+					"article");
+		} else {
+			std::tie(formatted_text, num_lines) =
+				item_renderer::to_stfl_list(
+					// cfg can't be nullptr because that's a long-lived object
+					// created at the very start of the program.
+					*cfg,
+					item,
+					text_width,
+					window_width,
+					rxman,
+					"article");
+		}
+
 		f->modify("article", "replace_inner", formatted_text);
 		f->set("articleoffset", "0");
 
@@ -449,27 +453,6 @@ void ItemViewFormAction::set_head(const std::string& s,
 	f->set("head",
 		fmt.do_format(
 			cfg->get_configvalue("itemview-title-format"), width));
-}
-
-void ItemViewFormAction::render_source(
-	std::vector<std::pair<LineType, std::string>>& lines,
-	std::string source)
-{
-	/*
-	 * This function is called instead of HtmlRenderer::render() when the
-	 * user requests to have the source displayed instead of seeing the
-	 * rendered HTML.
-	 */
-	std::string line;
-	do {
-		std::string::size_type pos = source.find_first_of("\r\n");
-		line = source.substr(0, pos);
-		if (pos == std::string::npos)
-			source.erase();
-		else
-			source.erase(0, pos + 1);
-		lines.push_back(std::make_pair(LineType::softwrappable, line));
-	} while (source.length() > 0);
 }
 
 void ItemViewFormAction::handle_cmdline(const std::string& cmd)

commit 8f744697d225ae518ee1ff06e14db1256ed5ba99

Fix 0..9 keys in article view
In article view, numeric keys open a URL with a given number. I broke
that behaviour in 485082f43f49f1c30974621107b1fa197618bf67 because
I didn't fill articleview's `links` field, so the view thought that
there are no links to open.

Thanks to @j605 for reporting this on IRC!

include/itemrenderer.h
@@ -32,14 +32,17 @@ namespace item_renderer {
 	/// `html-renderer` settings controls what tool is used to render HTML. \a
 	/// text_width dictates where text is wrapped. \a window_width dictates
 	/// where URLs are wrapped. \a rxman rules for \a location are used to
-	/// highlight the resulting text.
+	/// highlight the resulting text. \a links is filled with all the links
+	/// found in the article (which is useful for article view, which lets
+	/// users open the links by their number).
 	std::pair<std::string, size_t> to_stfl_list(
 			ConfigContainer& cfg,
 			std::shared_ptr<RssItem> item,
 			unsigned int text_width,
 			unsigned int window_width,
 			RegexManager* rxman,
-			const std::string& location);
+			const std::string& location,
+			std::vector<LinkPair>& links);
 
 	/// \brief Returns RssItem's text source as STFL list.
 	///

src/itemrenderer.cpp
@@ -141,10 +141,10 @@ std::pair<std::string, size_t> item_renderer::to_stfl_list(
 		unsigned int text_width,
 		unsigned int window_width,
 		RegexManager* rxman,
-		const std::string& location)
+		const std::string& location,
+		std::vector<LinkPair>& links)
 {
 	std::vector<std::pair<LineType, std::string>> lines;
-	std::vector<LinkPair> links;
 
 	prepare_header(item, lines, links);
 	const std::string baseurl = get_item_base_link(item);

src/itemviewformaction.cpp
@@ -125,7 +125,8 @@ void ItemViewFormAction::prepare()
 					text_width,
 					window_width,
 					rxman,
-					"article");
+					"article",
+					links);
 		}
 
 		f->modify("article", "replace_inner", formatted_text);