d12d3b959f03ebc017e675d61b01d54417322db4
Alexander Batischev <eual.jp@gmail.com>
2019-04-27 18:13:20 +0300

    Require &str as first argument to Rust strprintf::fmt

--- rust/strprintf/src/lib.rs
+++ rust/strprintf/src/lib.rs
@@ -1,14 +1,15 @@
 macro_rules! fmt {
-    ( $format:expr ) => {
-        $format
-    };
+    ( $format:expr ) => {{
+        let result: &str = $format;
+        result
+    }};
 }
 
 #[cfg(test)]
 mod tests {
     #[test]
     fn returns_first_argument_if_it_is_the_only_one() {
-        let input = "Hello, world!";
-        assert_eq!(fmt!(input), input);
+        let input = String::from("Hello, world!");
+        assert_eq!(fmt!(&input), input);
     }
 }
