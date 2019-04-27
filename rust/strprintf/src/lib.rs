e94b2e79fe63f1fd182d8dc9c860a1d6158828a6
Alexander Batischev <eual.jp@gmail.com>
2019-04-27 18:05:03 +0300

    Make Rust strprintf::fmt work for one argument

--- rust/strprintf/src/lib.rs
+++ rust/strprintf/src/lib.rs
@@ -1,7 +1,14 @@
+macro_rules! fmt {
+    ( $format:expr ) => {
+        $format
+    };
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn returns_first_argument_if_it_is_the_only_one() {
+        let input = "Hello, world!";
+        assert_eq!(fmt!(input), input);
     }
 }
