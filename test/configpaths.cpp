f284a70cdefdf13d5ca2e20cbb0d5c460ab0be7d
Alexander Batischev <eual.jp@gmail.com>
2019-05-10 19:41:37 +0300

    Test Configpaths::setup_dirs return code

--- test/configpaths.cpp
+++ test/configpaths.cpp
@@ -129,3 +129,204 @@ TEST_CASE("ConfigPaths::set_cache_file changes paths to cache and lock files",
 	REQUIRE(paths.cache_file() == new_cache);
 	REQUIRE(paths.lock_file() == new_cache + ".lock");
 }
+
+TEST_CASE("ConfigPaths::setup_dirs() returns true if both config and data dirs "
+		"were successfully created or already existed", "[ConfigPaths]")
+{
+	TestHelpers::TempDir tmp;
+
+	TestHelpers::EnvVar home("HOME");
+	home.set(tmp.getPath());
+	INFO("Temporary directory (used as HOME): " << tmp.getPath());
+
+	const auto require_exists = [&tmp](std::vector<std::string> dirs) {
+		ConfigPaths paths;
+		REQUIRE(paths.initialized());
+
+		const auto starts_with =
+		[]
+		(const std::string& input, const std::string& prefix) -> bool {
+			return input.substr(0, prefix.size()) == prefix;
+		};
+		REQUIRE(starts_with(paths.url_file(), tmp.getPath()));
+		REQUIRE(starts_with(paths.config_file(), tmp.getPath()));
+		REQUIRE(starts_with(paths.cache_file(), tmp.getPath()));
+		REQUIRE(starts_with(paths.lock_file(), tmp.getPath()));
+		REQUIRE(starts_with(paths.queue_file(), tmp.getPath()));
+		REQUIRE(starts_with(paths.search_file(), tmp.getPath()));
+		REQUIRE(starts_with(paths.cmdline_file(), tmp.getPath()));
+
+		REQUIRE(paths.setup_dirs());
+
+		for (const auto& dir : dirs) {
+			INFO("Checking directory " << dir);
+			const auto dir_exists = 0 == ::access(dir.c_str(), R_OK | X_OK);
+			REQUIRE(dir_exists);
+		}
+	};
+
+	const auto dotdir = tmp.getPath() + ".newsboat/";
+	const auto default_config_dir = tmp.getPath() + ".config/newsboat/";
+	const auto default_data_dir = tmp.getPath() + ".local/share/newsboat/";
+
+	SECTION("Using dotdir") {
+		SECTION("Dotdir didn't exist") {
+			require_exists({dotdir});
+		}
+
+		SECTION("Dotdir already existed") {
+			INFO("Creating " << dotdir);
+			REQUIRE(utils::mkdir_parents(dotdir, 0700) == 0);
+			require_exists({dotdir});
+		}
+	}
+
+	SECTION("Using XDG dirs") {
+		SECTION("No XDG environment variables") {
+			TestHelpers::EnvVar xdg_config_home("XDG_CONFIG_HOME");
+			xdg_config_home.unset();
+			TestHelpers::EnvVar xdg_data_home("XDG_DATA_HOME");
+			xdg_data_home.unset();
+
+			SECTION("No dirs existed => dotdir created") {
+				require_exists({dotdir});
+			}
+
+			SECTION("Config dir existed, data dir didn't => data dir created") {
+				INFO("Creating " << default_config_dir);
+				REQUIRE(utils::mkdir_parents(default_config_dir, 0700) == 0);
+				require_exists({default_config_dir, default_data_dir});
+			}
+
+			SECTION("Data dir existed, config dir didn't => dotdir created") {
+				INFO("Creating " << default_data_dir);
+				REQUIRE(utils::mkdir_parents(default_data_dir, 0700) == 0);
+				require_exists({dotdir});
+			}
+
+			SECTION("Both config and data dir existed => just returns true") {
+				INFO("Creating " << default_config_dir);
+				REQUIRE(utils::mkdir_parents(default_config_dir, 0700) == 0);
+				INFO("Creating " << default_data_dir);
+				REQUIRE(utils::mkdir_parents(default_data_dir, 0700) == 0);
+				require_exists({default_config_dir, default_data_dir});
+			}
+		}
+
+		SECTION("XDG_CONFIG_HOME redefined") {
+			const auto config_home =
+				tmp.getPath() + "config" + std::to_string(rand()) + "/";
+			INFO("Config home is " << config_home);
+			const auto config_dir = config_home + "/newsboat";
+
+			TestHelpers::EnvVar xdg_config_home("XDG_CONFIG_HOME");
+			xdg_config_home.set(config_home);
+
+			TestHelpers::EnvVar xdg_data_home("XDG_DATA_HOME");
+			xdg_data_home.unset();
+
+			SECTION("No dirs existed => dotdir created") {
+				require_exists({dotdir});
+			}
+
+			SECTION("Config dir existed, data dir didn't => data dir created") {
+				INFO("Creating " << config_dir);
+				REQUIRE(utils::mkdir_parents(config_dir, 0700) == 0);
+				require_exists({config_dir, default_data_dir});
+			}
+
+			SECTION("Data dir existed, config dir didn't => dotdir created") {
+				INFO("Creating " << default_data_dir);
+				REQUIRE(utils::mkdir_parents(default_data_dir, 0700) == 0);
+				require_exists({dotdir});
+			}
+
+			SECTION("Both config and data dir existed => just returns true") {
+				INFO("Creating " << config_dir);
+				REQUIRE(utils::mkdir_parents(config_dir, 0700) == 0);
+				INFO("Creating " << default_data_dir);
+				REQUIRE(utils::mkdir_parents(default_data_dir, 0700) == 0);
+				require_exists({config_dir, default_data_dir});
+			}
+		}
+
+		SECTION("XDG_DATA_HOME redefined") {
+			TestHelpers::EnvVar xdg_config_home("XDG_CONFIG_HOME");
+			xdg_config_home.unset();
+
+			const auto data_home =
+				tmp.getPath() + "data" + std::to_string(rand()) + "/";
+			INFO("Data home is " << data_home);
+			const auto data_dir = data_home + "/newsboat";
+
+			TestHelpers::EnvVar xdg_data_home("XDG_DATA_HOME");
+			xdg_data_home.set(data_home);
+
+			SECTION("No dirs existed => dotdir created") {
+				require_exists({dotdir});
+			}
+
+			SECTION("Config dir existed, data dir didn't => data dir created") {
+				INFO("Creating " << default_config_dir);
+				REQUIRE(utils::mkdir_parents(default_config_dir, 0700) == 0);
+				require_exists({default_config_dir, data_dir});
+			}
+
+			SECTION("Data dir existed, config dir didn't => dotdir created") {
+				INFO("Creating " << data_dir);
+				REQUIRE(utils::mkdir_parents(data_dir, 0700) == 0);
+				require_exists({dotdir});
+			}
+
+			SECTION("Both config and data dir existed => just returns true") {
+				INFO("Creating " << default_config_dir);
+				REQUIRE(utils::mkdir_parents(default_config_dir, 0700) == 0);
+				INFO("Creating " << data_dir);
+				REQUIRE(utils::mkdir_parents(data_dir, 0700) == 0);
+				require_exists({default_config_dir, data_dir});
+			}
+		}
+
+		SECTION("Both XDG_CONFIG_HOME and XDG_DATA_HOME redefined") {
+			const auto config_home =
+				tmp.getPath() + "config" + std::to_string(rand()) + "/";
+			INFO("Config home is " << config_home);
+			const auto config_dir = config_home + "/newsboat";
+
+			const auto data_home =
+				tmp.getPath() + "data" + std::to_string(rand()) + "/";
+			INFO("Data home is " << data_home);
+			const auto data_dir = data_home + "/newsboat";
+
+			TestHelpers::EnvVar xdg_config_home("XDG_CONFIG_HOME");
+			xdg_config_home.set(config_home);
+
+			TestHelpers::EnvVar xdg_data_home("XDG_DATA_HOME");
+			xdg_data_home.set(data_home);
+
+			SECTION("No dirs existed => dotdir created") {
+				require_exists({dotdir});
+			}
+
+			SECTION("Config dir existed, data dir didn't => data dir created") {
+				INFO("Creating " << config_dir);
+				REQUIRE(utils::mkdir_parents(config_dir, 0700) == 0);
+				require_exists({config_dir, data_dir});
+			}
+
+			SECTION("Data dir existed, config dir didn't => dotdir created") {
+				INFO("Creating " << data_dir);
+				REQUIRE(utils::mkdir_parents(data_dir, 0700) == 0);
+				require_exists({dotdir});
+			}
+
+			SECTION("Both config and data dir existed => just returns true") {
+				INFO("Creating " << config_dir);
+				REQUIRE(utils::mkdir_parents(config_dir, 0700) == 0);
+				INFO("Creating " << data_dir);
+				REQUIRE(utils::mkdir_parents(data_dir, 0700) == 0);
+				require_exists({config_dir, data_dir});
+			}
+		}
+	}
+}
