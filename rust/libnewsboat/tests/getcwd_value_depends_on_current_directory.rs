12439604ae67e66edc5a256443c6bf93bb114a77
Alexander Batischev <eual.jp@gmail.com>
2019-07-21 14:41:06 +0300

    fixup! Port `utils::getcwd` tests to Rust

--- rust/libnewsboat/tests/getcwd_value_depends_on_current_directory.rs
+++ rust/libnewsboat/tests/getcwd_value_depends_on_current_directory.rs
@@ -7,16 +7,17 @@ use std::env;
 
 #[test]
 fn t_getcwd_value_depends_on_current_directory() {
-    let initial_dir = utils::getcwd();
-    assert!(initial_dir.is_ok());
+    let initial_dir = utils::getcwd().unwrap();
 
     let tmp = TempDir::new().unwrap();
-    assert_ne!(initial_dir.unwrap(), tmp.path());
+    assert_ne!(&initial_dir, tmp.path());
     let chdir_result = env::set_current_dir(&tmp.path());
     assert!(chdir_result.is_ok());
 
-    let new_dir = utils::getcwd();
-    assert!(new_dir.is_ok());
+    let new_dir = utils::getcwd().unwrap();
 
-    assert_eq!(new_dir.unwrap(), tmp.path());
+    // We cannot assert that newdir == tmp.path() because the latter might contain symlinks, which
+    // getcwd() resolves. So we have to test a slightly weaker property: changing the current
+    // directory changes the return value of getcwd
+    assert_ne!(initial_dir, new_dir);
 }
