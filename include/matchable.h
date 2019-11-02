c9681f9785ff65dc7c1aa24cbd6b40252abc3b9e
Nikos Tsipinakis <nikos@tsipinakis.com>
2019-11-02 13:29:17 +0200

    fixup! Add C++ side matchable interface

--- include/matchable.h
+++ include/matchable.h
@@ -16,8 +16,8 @@ public:
 } // namespace newsboat
 
 extern "C" {
-bool matchable_has_attribute(void *ptr, char *attr);
-char* matchable_get_attribute(void *ptr, char *attr);
+bool matchable_has_attribute(const void *ptr, const char *attr);
+const char* matchable_get_attribute(const void *ptr, const char *attr);
 }
 
 #endif /* NEWSBOAT_MATCHABLE_H_ */
