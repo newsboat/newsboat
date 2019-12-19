2f263eb22289f7d83f2fec9377ee63eb7f269d55
Alexander Batischev <eual.jp@gmail.com>
2019-12-19 22:05:24 +0300

    Add Travis job that reports Rust test coverage
    This replaces cpp-coveralls tool (using gcov under the hood) with grcov
    and curl. There are both up- and down-sides with this.
    
    Upsides
    =======
    
    - grcov creates a single report for C++ and Rust.
    
    Downsides
    =========
    
    - grcov only supports LLVM's coverage data -- GCC builds no longer
      participate in coverage reports.
    
    - rustc support for coverage is not yet stable -- only Nightly Rust
      generates coverage.
    
      Also, Nightly Rust is a less stable of the branches, so we might have
      occasional problems with coverage. Eventually I'll look into pinning
      the Nightly version to avoid this, but I'm exhausted with this
      particular problem for now.
    
    - Coveralls reports don't have consecutive numbers anymore -- we now use
      Travis build IDs, which are long integers.
    
    - Coveralls reports no longer contain commit's message and author.
      I think this can be fixed in grcov itself.
    
    Fixes #346.

--- submit-to-coveralls.sh
+++ submit-to-coveralls.sh
@@ -1,12 +1,41 @@
 #!/bin/sh
 
-if [ "$TRAVIS_OS_NAME" = "osx" ]
+# For pull requests, TRAVIS_BRANCH is the *target* branch, usually "master".
+# This leads to mis-assigned coverage reports: they aren't assigned to PR's
+# source branch. To combat this, we detect if the build is triggered by a PR,
+# and explicitly use PR's source branch if so. That branch might not actually
+# exist in our main repo, but that's fine -- Coveralls doesn't check.
+#
+# Similar distinction exists between TRAVIS_COMMIT and TRAVIS_PULL_REQUEST_SHA,
+# but we don't paper over it there because we *do* want the report to attach to
+# the commit that was actually built: this way, we (theoretically) can notice
+# if the merge commit affected our coverage somehow.
+if [ "${TRAVIS_EVENT_TYPE}" = "pull_request" ]
 then
-    eval "$(pyenv init -)"
+    branch="${TRAVIS_PULL_REQUEST_BRANCH}"
+else
+    branch="${TRAVIS_BRANCH}"
 fi
 
-coveralls \
-    --exclude 'test' \
-    --exclude 'usr' \
-    --exclude '3rd-party' \
-    --gcov "$GCOV" --gcov-options '\-lp'
+${HOME}/.cargo/bin/grcov \
+    . \
+    --service-name "travis-ci" \
+    --service-number "${TRAVIS_BUILD_ID}" \
+    --service-job-number "${TRAVIS_JOB_ID}" \
+    --token "${COVERALLS_REPO_TOKEN}" \
+    --commit-sha "${TRAVIS_COMMIT}" \
+    --vcs-branch "${branch}" \
+    --ignore-not-existing \
+    --ignore='/*' \
+    --ignore='3rd-party/*' \
+    --ignore='doc/*' \
+    --ignore='test/*' \
+    --ignore='newsboat.cpp' \
+    --ignore='podboat.cpp' \
+    -t coveralls \
+    -o coveralls.json
+
+curl \
+    --form "json_file=@coveralls.json" \
+    --include \
+    https://coveralls.io/api/v1/jobs
