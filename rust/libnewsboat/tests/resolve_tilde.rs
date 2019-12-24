925cddaa4dada5a3eb0e597113954f3a02570e1b
Alexander Batischev <eual.jp@gmail.com>
2019-12-24 22:50:52 +0300

    Synchronize C++ and Rust tests for utils::resolve_tilde

--- rust/libnewsboat/tests/resolve_tilde.rs
+++ rust/libnewsboat/tests/resolve_tilde.rs
@@ -8,6 +8,8 @@ fn t_resolve_tilde() {
     env::set_var("HOME", "test");
     assert_eq!(utils::resolve_tilde(String::from("~")), "test");
     assert_eq!(utils::resolve_tilde(String::from("~/")), "test/");
+    assert_eq!(utils::resolve_tilde(String::from("~/dir")), "test/dir");
+    assert_eq!(utils::resolve_tilde(String::from("/home/~")), "/home/~");
     assert_eq!(
         utils::resolve_tilde(String::from("~/foo/bar")),
         "test/foo/bar"
--- test/utils.cpp
+++ test/utils.cpp
@@ -462,6 +462,11 @@ TEST_CASE("resolve_tilde() replaces ~ with the path to the $HOME directory",
 	REQUIRE(utils::resolve_tilde("~/") == "test/");
 	REQUIRE(utils::resolve_tilde("~/dir") == "test/dir");
 	REQUIRE(utils::resolve_tilde("/home/~") == "/home/~");
+	REQUIRE(
+		utils::resolve_tilde("~/foo/bar") ==
+		"test/foo/bar"
+	);
+	REQUIRE(utils::resolve_tilde("/foo/bar") == "/foo/bar");
 }
 
 TEST_CASE("resolve_relative() returns an absolute file path relative to another",
