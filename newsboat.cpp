ee827303e0762302f388fec0c29cc4c3e7ff410b
dennisschagt <dennisschagt@gmail.com>
2020-01-11 15:44:49 +0100

    Update copyright year 2019->2020

--- newsboat.cpp
+++ newsboat.cpp
@@ -123,11 +123,11 @@ void print_version(const std::string& argv0, unsigned int level)
 			<< PROGRAM_URL << std::endl;
 		ss << "Copyright (C) 2006-2015 Andreas Krennmair"
 			<< std::endl;
-		ss << "Copyright (C) 2015-2019 Alexander Batischev"
+		ss << "Copyright (C) 2015-2020 Alexander Batischev"
 			<< std::endl;
 		ss << "Copyright (C) 2006-2017 Newsbeuter contributors"
 			<< std::endl;
-		ss << "Copyright (C) 2017-2019 Newsboat contributors"
+		ss << "Copyright (C) 2017-2020 Newsboat contributors"
 			<< std::endl;
 		ss << std::endl;
 
