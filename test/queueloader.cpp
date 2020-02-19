7100b2956b792a908f5c7de7634ed286943ded7f
dennisschagt <dennisschagt@gmail.com>
2020-02-19 19:52:07 +0100

    Remove repetition from QueueLoader test; test true and false for reload

--- test/queueloader.cpp
+++ test/queueloader.cpp
@@ -1,5 +1,7 @@
 #include "queueloader.h"
 
+#include <fstream>
+
 #include "3rd-party/catch.hpp"
 #include "configcontainer.h"
 #include "download.h"
@@ -7,72 +9,73 @@
 
 using namespace podboat;
 
-TEST_CASE("reload(also_remove_finished == true) removes files iff they are marked as finished or deleted",
+bool contains_download_with_status(std::vector<Download>& downloads,
+	DlStatus status)
+{
+	for (const Download& d : downloads) {
+		if (d.status() == status) {
+			return true;
+		}
+	}
+	return false;
+}
+
+TEST_CASE("reload removes downloads iff they are marked as finished or deleted",
 	"[QueueLoader]")
 {
-	auto emptyCallback = []() {};
+	auto empty_callback = []() {};
 	newsboat::ConfigContainer cfg;
 	TestHelpers::TempFile queueFile;
 
-	std::vector<Download> downloads;
-	downloads.emplace_back(emptyCallback);
+	QueueLoader queue_loader(queueFile.get_path(), cfg, empty_callback);
 
-	QueueLoader queue_loader(queueFile.get_path(), cfg, emptyCallback);
+	std::vector<DlStatus> possible_statuses = {
+		DlStatus::QUEUED,
+		DlStatus::CANCELLED,
+		DlStatus::DELETED,
+		DlStatus::FINISHED,
+		DlStatus::FAILED,
+		DlStatus::ALREADY_DOWNLOADED,
+		DlStatus::READY,
+		DlStatus::PLAYED
+		// Not including DlStatus::Downloading because that aborts the reload
+	};
 
-	REQUIRE(downloads.size() == 1);
+	GIVEN("A vector with a download in every possible state") {
+		std::vector<Download> downloads;
+		for (DlStatus status : possible_statuses) {
+			downloads.emplace_back(empty_callback);
+			downloads.back().set_status(status);
+		}
 
-	SECTION("reload removes download marked as DELETED") {
-		downloads.back().set_status(DlStatus::DELETED);
-		queue_loader.reload(downloads, true);
-		REQUIRE(downloads.size() == 0);
-	}
+		WHEN("reload() is called with also_remove_finished == false") {
+			queue_loader.reload(downloads, false);
 
-	SECTION("reload removes download marked as FINISHED") {
-		downloads.back().set_status(DlStatus::FINISHED);
-		queue_loader.reload(downloads, true);
-		REQUIRE(downloads.size() == 0);
-	}
+			THEN("only files marked as deleted are removed") {
+				for (DlStatus status : possible_statuses) {
+					INFO("status: " << static_cast<int>(status));
+					if (status == DlStatus::DELETED) {
+						REQUIRE_FALSE(contains_download_with_status(downloads, status));
+					} else {
+						REQUIRE(contains_download_with_status(downloads, status));
+					}
+				}
+			}
+		}
 
+		WHEN("reload() is called with also_remove_finished == true") {
+			queue_loader.reload(downloads, true);
 
-	SECTION("reload does not remove download marked as QUEUED") {
-		downloads.back().set_status(DlStatus::QUEUED);
-		queue_loader.reload(downloads, true);
-		REQUIRE(downloads.size() == 1);
-	}
-
-	SECTION("reload does not remove download marked as DOWNLOADING") {
-		downloads.back().set_status(DlStatus::DOWNLOADING);
-		queue_loader.reload(downloads, true);
-		REQUIRE(downloads.size() == 1);
-	}
-
-	SECTION("reload does not remove download marked as CANCELLED") {
-		downloads.back().set_status(DlStatus::CANCELLED);
-		queue_loader.reload(downloads, true);
-		REQUIRE(downloads.size() == 1);
-	}
-
-	SECTION("reload does not remove download marked as FAILED") {
-		downloads.back().set_status(DlStatus::FAILED);
-		queue_loader.reload(downloads, true);
-		REQUIRE(downloads.size() == 1);
-	}
-
-	SECTION("reload does not remove download marked as ALREADY_DOWNLOADED") {
-		downloads.back().set_status(DlStatus::ALREADY_DOWNLOADED);
-		queue_loader.reload(downloads, true);
-		REQUIRE(downloads.size() == 1);
-	}
-
-	SECTION("reload does not remove download marked as READY") {
-		downloads.back().set_status(DlStatus::READY);
-		queue_loader.reload(downloads, true);
-		REQUIRE(downloads.size() == 1);
-	}
-
-	SECTION("reload does not remove download marked as PLAYED") {
-		downloads.back().set_status(DlStatus::PLAYED);
-		queue_loader.reload(downloads, true);
-		REQUIRE(downloads.size() == 1);
+			THEN("only files marked as finished or deleted are removed") {
+				for (DlStatus status : possible_statuses) {
+					INFO("status: " << static_cast<int>(status));
+					if (status == DlStatus::FINISHED || status == DlStatus::DELETED) {
+						REQUIRE_FALSE(contains_download_with_status(downloads, status));
+					} else {
+						REQUIRE(contains_download_with_status(downloads, status));
+					}
+				}
+			}
+		}
 	}
 }
