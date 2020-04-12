90f3cfe1f010c79997bccba1fcf77815f9b5c932
Dennis van der Schagt <dennisschagt@gmail.com>
2020-04-12 18:10:01 +0200

    Add tests to verify that FileUrlReader throws if file cannot be opened

--- test/fileurlreader.cpp
+++ test/fileurlreader.cpp
@@ -5,6 +5,7 @@
 #include <unistd.h>
 
 #include "3rd-party/catch.hpp"
+#include "test-helpers/chmod.h"
 #include "test-helpers/misc.h"
 #include "test-helpers/tempfile.h"
 
@@ -81,5 +82,61 @@ TEST_CASE("URL reader writes files that it can understand later",
 	REQUIRE(u.get_urls() == u2.get_urls());
 	for (const auto& url : u.get_urls()) {
 		REQUIRE(u.get_tags(url) == u2.get_tags(url));
+	}
+}
+
+TEST_CASE("URL reader throws exception if file cannot be opened",
+	"[FileUrlReader]")
+{
+	const std::string testDataPath("data/test-urls.txt");
+	TestHelpers::TempFile urlsFile;
+
+	FileUrlReader u(urlsFile.get_path());
+
+	SECTION("reload() and load_config() throw if file does not exist") {
+		REQUIRE_THROWS_AS(u.reload(), std::exception);
+		REQUIRE_THROWS_AS(u.load_config(urlsFile.get_path()), std::exception);
+	}
+
+	SECTION("write_config() works fine if file does not exist") {
+		REQUIRE_NOTHROW(u.write_config());
+
+		SECTION("after writing file, reload() and load_config() succeed") {
+			REQUIRE_NOTHROW(u.reload());
+			REQUIRE_NOTHROW(u.load_config(urlsFile.get_path()));
+		}
+	}
+
+	TestHelpers::copy_file(testDataPath, urlsFile.get_path());
+
+	SECTION("reload() and load_config() succeed if url file exists") {
+		REQUIRE_NOTHROW(u.reload());
+		REQUIRE_NOTHROW(u.load_config(urlsFile.get_path()));
+	}
+
+	GIVEN("that the urls file is not readable") {
+		TestHelpers::Chmod notReadable(urlsFile.get_path(), S_IWUSR);
+
+		THEN("reload() and load_config() throw") {
+			REQUIRE_THROWS_AS(u.reload(), std::exception);
+			REQUIRE_THROWS_AS(u.load_config(urlsFile.get_path()), std::exception);
+		}
+
+		THEN("write_config() still works fine") {
+			REQUIRE_NOTHROW(u.write_config());
+		}
+	}
+
+	GIVEN("that the urls file is not writable") {
+		TestHelpers::Chmod notWritable(urlsFile.get_path(), S_IRUSR);
+
+		THEN("write_config() throws") {
+			REQUIRE_THROWS_AS(u.write_config(), std::exception);
+		}
+
+		THEN("reload() and load_config() still work fine") {
+			REQUIRE_NOTHROW(u.reload());
+			REQUIRE_NOTHROW(u.load_config(urlsFile.get_path()));
+		}
 	}
 }
