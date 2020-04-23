bba18a992d20329c9dbb1f87950741b5570c69f1
Leo <thinkabit.ukim@gmail.com>
2020-04-22 23:17:23 -0300

    Adapt to json-c-0.14 API changes
    TRUE and FALSE don't exist anymore and are '1' and '0' respectively

--- src/newsblurapi.cpp
+++ src/newsblurapi.cpp
@@ -178,7 +178,7 @@ void NewsBlurApi::add_custom_headers(curl_slist** /* custom_headers */)
 bool request_successfull(json_object* payload)
 {
 	json_object* result{};
-	if (json_object_object_get_ex(payload, "result", &result) == FALSE) {
+	if (json_object_object_get_ex(payload, "result", &result) == 0) {
 		return false;
 	} else {
 		return !strcmp("ok", json_object_get_string(result));
@@ -254,7 +254,7 @@ rsspp::Feed NewsBlurApi::fetch_feed(const std::string& id)
 
 		json_object* stories{};
 		if (json_object_object_get_ex(
-				query_result, "stories", &stories) == FALSE) {
+				query_result, "stories", &stories) == 0) {
 			LOG(Level::ERROR,
 				"NewsBlurApi::fetch_feed: request returned no "
 				"stories");
@@ -283,37 +283,37 @@ rsspp::Feed NewsBlurApi::fetch_feed(const std::string& id)
 			json_object* node{};
 
 			if (json_object_object_get_ex(
-					item_obj, "story_title", &node) == TRUE) {
+					item_obj, "story_title", &node) == 1) {
 				item.title = json_object_get_string(node);
 			}
 
 			if (json_object_object_get_ex(
-					item_obj, "story_authors", &node) == TRUE) {
+					item_obj, "story_authors", &node) == 1) {
 				item.author = json_object_get_string(node);
 			}
 
 			if (json_object_object_get_ex(item_obj,
 					"story_permalink",
-					&node) == TRUE) {
+					&node) == 1) {
 				item.link = json_object_get_string(node);
 			}
 
 			if (json_object_object_get_ex(
-					item_obj, "story_content", &node) == TRUE) {
+					item_obj, "story_content", &node) == 1) {
 				item.content_encoded =
 					json_object_get_string(node);
 			}
 
 			const char* article_id{};
 			if (json_object_object_get_ex(item_obj, "id", &node) ==
-				TRUE) {
+				1) {
 				article_id = json_object_get_string(node);
 			}
 			item.guid = id + ID_SEPARATOR +
 				(article_id ? article_id : "");
 
 			if (json_object_object_get_ex(
-					item_obj, "read_status", &node) == TRUE) {
+					item_obj, "read_status", &node) == 1) {
 				if (!static_cast<bool>(
 						json_object_get_int(node))) {
 					item.labels.push_back(
@@ -324,7 +324,7 @@ rsspp::Feed NewsBlurApi::fetch_feed(const std::string& id)
 			}
 
 			if (json_object_object_get_ex(
-					item_obj, "story_date", &node) == TRUE) {
+					item_obj, "story_date", &node) == 1) {
 				const char* pub_date =
 					json_object_get_string(node);
 				item.pubDate_ts = parse_date(pub_date);
