ee4f57409becbc578050a43f9c8a6c41d5b5c5d9
Ivan Tham <pickfire@riseup.net>
2020-05-07 15:34:49 +0800

    fixup! Port FsLock to Rust

--- rust/libnewsboat-ffi/src/fslock.rs
+++ rust/libnewsboat-ffi/src/fslock.rs
@@ -8,31 +8,31 @@ pub extern "C" fn rs_fslock_new() -> *mut FsLock {
 }
 
 #[no_mangle]
-pub extern "C" fn rs_fslock_free(ptr: *mut FsLock) {
+pub unsafe extern "C" fn rs_fslock_free(ptr: *mut FsLock) {
     abort_on_panic(|| {
         if ptr.is_null() {
             return;
         }
-        unsafe { Box::from_raw(ptr) };
+        Box::from_raw(ptr);
     })
 }
 
 #[no_mangle]
-pub extern "C" fn rs_fslock_try_lock(
+pub unsafe extern "C" fn rs_fslock_try_lock(
     ptr: *mut FsLock,
     new_lock_filepath: *const libc::c_char,
     pid: *mut libc::pid_t,
 ) -> bool {
     abort_on_panic(|| {
-        let fslock = unsafe {
+        let fslock = {
             assert!(!ptr.is_null());
             &mut *ptr
         };
-        let new_lock_filepath = unsafe {
+        let new_lock_filepath = {
             assert!(!new_lock_filepath.is_null());
             CStr::from_ptr(new_lock_filepath)
         };
-        let pid = unsafe { &mut *pid };
+        let pid = { &mut *pid };
         let new_lock_filepath = new_lock_filepath.to_string_lossy().into_owned();
         fslock.try_lock(new_lock_filepath.as_ref(), pid)
     })
