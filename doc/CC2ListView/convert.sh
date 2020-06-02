51413da4e479a11e596cb8a51bfd176100a7445b
Spacewalker2 <buht@u53r.space>
2020-06-02 17:15:59 +0200

    Changed to general AWK execution.

--- doc/CC2ListView/convert.sh
+++ doc/CC2ListView/convert.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
-sed 's/||/\t/g' $1 > ${1/dsv/tsv}
-gawk -f doc/CC2ListView/convertTable.awk ${1/dsv/tsv}
+
+if [ $# -eq 1 ]
+then
+	sed 's/||/\t/g' $1 > ${1/dsv/tsv}
+	awk -f doc/CC2ListView/convertTable.awk ${1/dsv/tsv}
+else
+        echo "USAGE: $0 <dsv-file>"
+fi
