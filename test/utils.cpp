newsboat/newsboat

commit 4b327fbff264c69bb78765304e16c2403c65b5e1

tokenize_quoted: simplify by avoiding double-pass
As suggested by @dennisschagt during review:
https://github.com/newsboat/newsboat/pull/929#discussion_r422969588.

src/utils.cpp
@@ -121,31 +121,21 @@ std::vector<std::string> utils::tokenize_quoted(const std::string& str,
 		if (str[last_pos] == '"') {
 			++last_pos;
 			pos = last_pos;
-			// Is the character at position `pos` escaped, i.e. is `str[pos-1]
-			// == '\\'`? We can't actually use that char comparison because
-			// `pos` might be zero.
-			bool is_escaped = false;
-			while (pos < str.length() && !(str[pos] == '"' && !is_escaped)) {
-				if (str[pos] == '\\') {
-					is_escaped = !is_escaped;
-				} else {
-					is_escaped = false;
-				}
-				++pos;
-			}
-
-			// Invariant: pos <= str.length()
 
 			std::string token;
-			while (last_pos < pos) {
-				if (str[last_pos] == '\\') {
-					if (last_pos + 1 < str.length()) {
-						append_escapes(token, str[last_pos + 1]);
+			while (pos < str.length()) {
+				if (str[pos] == '"') {
+					// We've reached the end of this quoted token.
+					++pos;
+					break;
+				} else if (str[pos] == '\\') {
+					if (pos + 1 < str.length()) {
+						append_escapes(token, str[pos + 1]);
 					}
-					last_pos += 2;
+					pos += 2;
 				} else {
-					token.push_back(str[last_pos]);
-					++last_pos;
+					token.push_back(str[pos]);
+					++pos;
 				}
 			}
 			tokens.push_back(token);
@@ -159,6 +149,7 @@ std::vector<std::string> utils::tokenize_quoted(const std::string& str,
 			pos = str.find_first_of(delimiters, last_pos);
 			tokens.push_back(str.substr(last_pos, pos - last_pos));
 		}
+
 		last_pos = str.find_first_not_of(delimiters, pos);
 	}
 

commit 4051b61b7798e1c7ba225795d78845678469fa94

Fix #1013
4b327fbff264c69bb78765304e16c2403c65b5e1 (#929) introduced a bug which
made utils::tokenize_quoted() skip the character that follows the
closing double quote. This surfaced as #1013, where Newsboat fails to
extract the semicolon from:

    set browser "whatever"; some-other-token

The problem was caused by incrementing the position twice.

src/utils.cpp
@@ -142,8 +142,6 @@ std::vector<std::string> utils::tokenize_quoted(const std::string& str,
 
 			if (pos == str.length()) {
 				pos = std::string::npos;
-			} else {
-				++pos;
 			}
 		} else {
 			pos = str.find_first_of(delimiters, last_pos);

test/utils.cpp
@@ -98,6 +98,16 @@ TEST_CASE(
 		REQUIRE(tokens[0] == "foo \\xxx");
 		REQUIRE(tokens[1] == " ");
 	}
+
+	SECTION("Closing double quote marks the end of a token") {
+		tokens = utils::tokenize_quoted(R"(set browser "mpv %u";)");
+
+		REQUIRE(tokens.size() == 4);
+		REQUIRE(tokens[0] == "set");
+		REQUIRE(tokens[1] == "browser");
+		REQUIRE(tokens[2] == "mpv %u");
+		REQUIRE(tokens[3] == ";");
+	}
 }
 
 TEST_CASE("tokenize_quoted() implicitly closes quotes at the end of the string",