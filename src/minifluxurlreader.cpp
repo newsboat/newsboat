5910794bca5b24bc224e2a4323c208f89e641db6
Galen Abell <galen@galenabell.com>
2020-08-06 16:53:46 +0200

    Add MinifluxUrlReader

---
+++ src/minifluxurlreader.cpp
@@ -0,0 +1,52 @@
+#include "minifluxurlreader.h"
+
+#include "fileurlreader.h"
+#include "logger.h"
+#include "remoteapi.h"
+#include "utils.h"
+
+namespace newsboat {
+
+MinifluxUrlReader::MinifluxUrlReader(const std::string& url_file, RemoteApi* a)
+	: file(url_file)
+	, api(a)
+{
+}
+
+MinifluxUrlReader::~MinifluxUrlReader() {}
+
+void MinifluxUrlReader::reload()
+{
+	urls.clear();
+	tags.clear();
+	alltags.clear();
+
+	FileUrlReader ur(file);
+	ur.reload();
+
+	std::vector<std::string>& file_urls(ur.get_urls());
+	for (const auto& url : file_urls) {
+		if (utils::is_query_url(url)) {
+			urls.push_back(url);
+		}
+	}
+
+	std::vector<TaggedFeedUrl> feedurls = api->get_subscribed_urls();
+
+	for (const auto& url : feedurls) {
+		LOG(Level::INFO, "added %s to URL list", url.first);
+		urls.push_back(url.first);
+		tags[url.first] = url.second;
+		for (const auto& tag : url.second) {
+			LOG(Level::DEBUG, "%s: added tag %s", url.first, tag);
+			alltags.insert(tag);
+		}
+	}
+}
+
+std::string MinifluxUrlReader::get_source()
+{
+	return "Miniflux";
+}
+
+} // namespace newsboat
