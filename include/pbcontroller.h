newsboat/newsboat

commit 9c720413519aa29b1bb1ef3be9fff5e1696bba55

Apply effect of `scrolloff` option to podboat downloads list
Moving part of `PbController::run()` into an intialization function to
make sure the `ConfigContainer` is initalized before we use it in the
`PbView` constructor.

include/pbcontroller.h
@@ -5,9 +5,11 @@
 #include <string>
 #include <vector>
 
+#include "colormanager.h"
 #include "configcontainer.h"
 #include "download.h"
 #include "fslock.h"
+#include "keymap.h"
 #include "queueloader.h"
 
 namespace podboat {
@@ -24,7 +26,8 @@ public:
 	{
 		v = vv;
 	}
-	int run(int argc, char* argv[] = 0);
+	int initialize(int argc, char* argv[]);
+	int run();
 
 	bool view_update_necessary() const
 	{
@@ -82,6 +85,10 @@ private:
 
 	std::string lock_file;
 	std::unique_ptr<newsboat::FsLock> fslock;
+
+	bool automatic_dl = false;
+	newsboat::ColorManager colorman;
+	newsboat::KeyMap keys;
 };
 
 } // namespace podboat

podboat.cpp
@@ -27,12 +27,16 @@ int main(int argc, char* argv[])
 
 	PbController c;
 
-	int ret;
 	try {
+		int ret = c.initialize(argc, argv);
+		if (ret != EXIT_SUCCESS) {
+			return ret;
+		}
+
 		podboat::PbView v(&c);
 		c.set_view(&v);
 
-		ret = c.run(argc, argv);
+		return c.run();
 	} catch (const newsboat::Exception& e) {
 		std::cerr << strprintf::fmt(_("Caught newsboat::Exception with "
 					"message: %s"),
@@ -40,6 +44,4 @@ int main(int argc, char* argv[])
 			<< std::endl;
 		::exit(EXIT_FAILURE);
 	}
-
-	return ret;
 }

src/pbcontroller.cpp
@@ -12,12 +12,10 @@
 #include <thread>
 #include <unistd.h>
 
-#include "colormanager.h"
 #include "config.h"
 #include "configcontainer.h"
 #include "configexception.h"
 #include "globals.h"
-#include "keymap.h"
 #include "logger.h"
 #include "matcherexception.h"
 #include "nullconfigactionhandler.h"
@@ -135,6 +133,7 @@ PbController::PbController()
 	, max_dls(1)
 	, ql(0)
 	, lock_file("pb-lock.pid")
+	, keys(KM_PODBOAT)
 {
 	char* cfgdir;
 	if (!(cfgdir = ::getenv("HOME"))) {
@@ -186,10 +185,9 @@ PbController::~PbController()
 	delete cfg;
 }
 
-int PbController::run(int argc, char* argv[])
+int PbController::initialize(int argc, char* argv[])
 {
 	int c;
-	bool automatic_dl = false;
 
 	::signal(SIGINT, ctrl_c_action);
 
@@ -266,10 +264,8 @@ int PbController::run(int argc, char* argv[])
 	ConfigParser cfgparser;
 	cfg = new ConfigContainer();
 	cfg->register_commands(cfgparser);
-	ColorManager* colorman = new ColorManager();
-	colorman->register_commands(cfgparser);
+	colorman.register_commands(cfgparser);
 
-	KeyMap keys(KM_PODBOAT);
 	cfgparser.register_handler("bind-key", &keys);
 	cfgparser.register_handler("unbind-key", &keys);
 
@@ -287,14 +283,17 @@ int PbController::run(int argc, char* argv[])
 		cfgparser.parse(config_file);
 	} catch (const ConfigException& ex) {
 		std::cout << ex.what() << std::endl;
-		delete colorman;
 		return EXIT_FAILURE;
 	}
 
-	if (colorman->colors_loaded()) {
-		colorman->set_pb_colors(v);
+	return EXIT_SUCCESS;
+}
+
+int PbController::run()
+{
+	if (colorman.colors_loaded()) {
+		colorman.set_pb_colors(v);
 	}
-	delete colorman;
 
 	max_dls = cfg->get_configvalue_as_int("max-downloads");
 

mk/mk.deps
@@ -159,8 +159,9 @@ src/dirbrowserformaction.o: src/dirbrowserformaction.cpp \
  include/filebrowserformaction.h include/htmlrenderer.h \
  include/textformatter.h
 src/download.o: src/download.cpp include/download.h config.h \
- include/pbcontroller.h include/configcontainer.h include/configparser.h \
- include/configactionhandler.h include/download.h include/fslock.h \
+ include/pbcontroller.h include/colormanager.h include/configparser.h \
+ include/configactionhandler.h include/configcontainer.h \
+ include/download.h include/fslock.h include/keymap.h \
  include/queueloader.h
 src/downloadthread.o: src/downloadthread.cpp include/downloadthread.h \
  include/reloader.h include/configcontainer.h include/configparser.h \
@@ -422,18 +423,18 @@ src/opmlurlreader.o: src/opmlurlreader.cpp include/opmlurlreader.h \
  include/configactionhandler.h include/urlreader.h include/utils.h \
  3rd-party/optional.hpp include/logger.h config.h include/strprintf.h
 src/pbcontroller.o: src/pbcontroller.cpp include/pbcontroller.h \
- include/configcontainer.h include/configparser.h \
- include/configactionhandler.h include/download.h include/fslock.h \
- include/queueloader.h include/colormanager.h config.h \
- include/configcontainer.h include/configexception.h include/globals.h \
- include/keymap.h include/logger.h include/strprintf.h \
- include/matcherexception.h include/nullconfigactionhandler.h \
- include/pbview.h include/colormanager.h include/keymap.h \
- include/listwidget.h include/listformatter.h include/regexmanager.h \
- include/matcher.h filter/FilterParser.h include/regexowner.h \
- include/stflpp.h include/textviewwidget.h include/poddlthread.h \
- include/queueloader.h include/strprintf.h include/utils.h \
- 3rd-party/optional.hpp include/logger.h
+ include/colormanager.h include/configparser.h \
+ include/configactionhandler.h include/configcontainer.h \
+ include/download.h include/fslock.h include/keymap.h \
+ include/queueloader.h config.h include/configcontainer.h \
+ include/configexception.h include/globals.h include/logger.h \
+ include/strprintf.h include/matcherexception.h \
+ include/nullconfigactionhandler.h include/pbview.h include/listwidget.h \
+ include/listformatter.h include/regexmanager.h include/matcher.h \
+ filter/FilterParser.h include/regexowner.h include/stflpp.h \
+ include/textviewwidget.h include/poddlthread.h include/queueloader.h \
+ include/strprintf.h include/utils.h 3rd-party/optional.hpp \
+ include/logger.h
 src/pbview.o: src/pbview.cpp include/pbview.h include/colormanager.h \
  include/configparser.h include/configactionhandler.h include/keymap.h \
  include/listwidget.h include/listformatter.h include/regexmanager.h \

src/pbview.cpp
@@ -29,8 +29,8 @@ PbView::PbView(PbController* c)
 	, dllist_form(dllist_str)
 	, help_form(help_str)
 	, keys(0)
-	  //, downloads_list("dls", dllist_form, ctrl->get_cfgcont()->get_configvalue_as_int("scrolloff"))
-	, downloads_list("dls", dllist_form, 0)
+	, downloads_list("dls", dllist_form,
+		  ctrl->get_cfgcont()->get_configvalue_as_int("scrolloff"))
 	, help_textview("helptext", help_form)
 {
 	if (getenv("ESCDELAY") == nullptr) {

commit a33ba8bec63fb5677cf581f1a44871ee628ba092

Fix segfault in podboat --help
9c720413519aa29b1bb1ef3be9fff5e1696bba55 introduced a bug: `podboat
--help` segfaults after printing out the help message.

That commit split out some initialization code from
`PbController::run()` into a new method, `initialize()`. That new method
didn't *always* initialize the object; if --help or an unknown option
was passed into Newsboat, the method would print out the usage message
and return EXIT_SUCCESS.

However, this same EXIT_SUCCESS value was used to indicate that the
initialization was successful. As a result, Podboat proceeded to use
unitialized PbController, and crashed.

To fix this, I changed `PbController::initialize()` to call `exit(3)`
directly when it wanted to quit Podboat.

include/pbcontroller.h
@@ -26,7 +26,7 @@ public:
 	{
 		v = vv;
 	}
-	int initialize(int argc, char* argv[]);
+	void initialize(int argc, char* argv[]);
 	int run();
 
 	bool view_update_necessary() const

podboat.cpp
@@ -28,10 +28,7 @@ int main(int argc, char* argv[])
 	PbController c;
 
 	try {
-		int ret = c.initialize(argc, argv);
-		if (ret != EXIT_SUCCESS) {
-			return ret;
-		}
+		c.initialize(argc, argv);
 
 		podboat::PbView v(&c);
 		c.set_view(&v);

src/pbcontroller.cpp
@@ -185,7 +185,7 @@ PbController::~PbController()
 	delete cfg;
 }
 
-int PbController::initialize(int argc, char* argv[])
+void PbController::initialize(int argc, char* argv[])
 {
 	int c;
 
@@ -209,7 +209,7 @@ int PbController::initialize(int argc, char* argv[])
 		case ':':
 		case '?':
 			print_usage(argv[0]);
-			return EXIT_FAILURE;
+			exit(EXIT_FAILURE);
 		case 'C':
 			config_file = optarg;
 			break;
@@ -232,13 +232,13 @@ int PbController::initialize(int argc, char* argv[])
 						argv[0],
 						static_cast<int>(l))
 					<< std::endl;
-				return EXIT_FAILURE;
+				exit(EXIT_FAILURE);
 			}
 		}
 		break;
 		case 'h':
 			print_usage(argv[0]);
-			return EXIT_SUCCESS;
+			exit(EXIT_SUCCESS);
 		}
 	};
 
@@ -255,7 +255,7 @@ int PbController::initialize(int argc, char* argv[])
 				"podboat",
 				pid)
 			<< std::endl;
-		return EXIT_FAILURE;
+		exit(EXIT_FAILURE);
 	}
 
 	std::cout << _("Loading configuration...");
@@ -283,10 +283,8 @@ int PbController::initialize(int argc, char* argv[])
 		cfgparser.parse_file(config_file);
 	} catch (const ConfigException& ex) {
 		std::cout << ex.what() << std::endl;
-		return EXIT_FAILURE;
+		exit(EXIT_FAILURE);
 	}
-
-	return EXIT_SUCCESS;
 }
 
 int PbController::run()