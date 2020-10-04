45f008fd250a84b4a56666f9a457682a651440cd
Tarishi Jain <t.jain@tjain-ltm1.internal.salesforce.com>
2020-10-04 23:07:16 +0530

    #1214 Add setting to skip confirmation of marking feeds read

--- src/feedlistformaction.cpp
+++ src/feedlistformaction.cpp
@@ -406,7 +406,9 @@ REDO:
 	}
 	break;
 	case OP_MARKALLFEEDSREAD:
-		if (v->confirm(_("Do you really want to mark all feeds as read (y:Yes n:No)? "),
+		if (!get_cfg()->get_configvalue_as_bool(
+					"confirm-markAllFeedsRead") ||
+			v->confirm(_("Do you really want to mark all feeds as read (y:Yes n:No)? "),
 				_("yn")) == *_("y")) {
 			LOG(Level::INFO, "FeedListFormAction: marking all feeds read");
 			v->set_status(_("Marking all feeds read..."));
