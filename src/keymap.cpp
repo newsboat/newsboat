874025a4c2282e5d7149fae231fbbc8e60e92bd8
anton <code@a1ro.com>
2020-10-04 22:13:40 -0400

    docs: open-in-browser corrections.

--- src/keymap.cpp
+++ src/keymap.cpp
@@ -119,7 +119,7 @@ static const std::vector<OpDesc> opdescs = {
 		OP_OPENBROWSER_AND_MARK,
 		"open-in-browser-and-mark-read",
 		"O",
-		_("Open URL of current article, or entry in URL view. Mark as read in article list"),
+		_("Open URL of article, or entry in URL view. Mark read."),
 		KM_ARTICLELIST | KM_ARTICLE | KM_URLVIEW
 	},
 	{
@@ -141,7 +141,7 @@ static const std::vector<OpDesc> opdescs = {
 		OP_OPENINBROWSER,
 		"open-in-browser",
 		"o",
-		_("Open URL of current article, feed or entry in URL view"),
+		_("Open URL of article, feed, or entry in URL view"),
 		KM_FEEDLIST | KM_ARTICLELIST | KM_ARTICLE | KM_URLVIEW
 	},
 	{
