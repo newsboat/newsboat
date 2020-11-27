34b9cafb84a947da580e6394c953334eadf18dbb
Alexander Batischev <eual.jp@gmail.com>
2020-11-27 19:53:28 +0300

    Demonstrate how to wrap a method for cxx

--- src/fslock.cpp
+++ src/fslock.cpp
@@ -18,7 +18,8 @@ FsLock::FsLock()
 bool FsLock::try_lock(const std::string& new_lock_filepath, pid_t& pid)
 {
 	std::int64_t p;
-	const bool result =  rs_object->try_lock_ffi(new_lock_filepath, p);
+	const bool result = newsboat::fslock::bridged::try_lock(*rs_object,
+			new_lock_filepath, p);
 
 	// We use `libc::pid_t` on the rust side so we can guarantee this will fit
 	pid = static_cast<std::int64_t>(p);
