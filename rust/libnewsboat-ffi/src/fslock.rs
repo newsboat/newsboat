e893a0a4e56a024b2f6d362b77d2c28e4286dca6
Dennis van der Schagt <dennisschagt@gmail.com>
2020-11-27 03:33:09 +0100

    Migrate FsLock rust binding to cxx crate

--- rust/libnewsboat-ffi/src/fslock.rs
+++ rust/libnewsboat-ffi/src/fslock.rs
@@ -1,39 +1,15 @@
-use crate::abort_on_panic;
 use libnewsboat::fslock::FsLock;
-use std::ffi::CStr;
 
-#[no_mangle]
-pub extern "C" fn rs_fslock_new() -> *mut FsLock {
-    Box::into_raw(Box::new(FsLock::default()))
+#[cxx::bridge(namespace = "newsboat::fslock::bridged")]
+mod bridged {
+    extern "Rust" {
+        type FsLock;
+
+        fn create() -> Box<FsLock>;
+        fn try_lock_ffi(&mut self, new_lock_path: &str, pid: &mut i64) -> bool;
+    }
 }
 
-#[no_mangle]
-pub unsafe extern "C" fn rs_fslock_free(ptr: *mut FsLock) {
-    abort_on_panic(|| {
-        if ptr.is_null() {
-            return;
-        }
-        Box::from_raw(ptr);
-    })
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn rs_fslock_try_lock(
-    ptr: *mut FsLock,
-    new_lock_filepath: *const libc::c_char,
-    pid: *mut libc::pid_t,
-) -> bool {
-    abort_on_panic(|| {
-        let fslock = {
-            assert!(!ptr.is_null());
-            &mut *ptr
-        };
-        let new_lock_filepath = {
-            assert!(!new_lock_filepath.is_null());
-            CStr::from_ptr(new_lock_filepath)
-        };
-        let pid = { &mut *pid };
-        let new_lock_filepath = new_lock_filepath.to_string_lossy().into_owned();
-        fslock.try_lock(new_lock_filepath.as_ref(), pid)
-    })
+fn create() -> Box<FsLock> {
+    Box::new(FsLock::default())
 }
