newsboat/newsboat

commit 54617872ff36d7420505e4c82cc08a14ed16f6c3

Work around -Wmaybe-uninitialized on GCC 9
This is a re-thinking of a454fff6a8b7456bd0d4c790c9911669daefa984 in
light of https://github.com/newsboat/newsboat/issues/1351. The problem
with that earlier commit is that `-Wno-unknown-warning-option` is only
supported by Clang. GCC treats it in a really bizarre way: it only
complains about unsupported warning when there are some other
diagnostics to report. Furthermore, if `-Werror` is in effect, there
seem to be no way to selectively demote the complaint back to
a warning — the only solution is `-Wno-error`.

So instead of playing with pragmas, I rewrote that code in a way that
doesn't trigger -Wmaybe-uninitialized. This is opportunistic, of
course — there is absolutely no guarantee that some future version of
some compiler, or even an existing compiler in a non-default build
configuration, won't emit the warning again. But we got CI checking the
default configuration now, so we should be able to catch the most
obvious cases.

src/itemlistformaction.cpp
@@ -167,7 +167,12 @@ bool ItemListFormAction::process_operation(Operation op,
 				"ItemListFormAction: opening all unread items "
 				"in "
 				"browser");
-			const auto exit_code = open_unread_items_in_browser(feed, false);
+
+			// We can't just `const auto exit_code = ...` here because this
+			// triggers -Wmaybe-initialized in GCC 9 with -O2.
+			nonstd::optional<std::uint8_t> exit_code;
+			exit_code = open_unread_items_in_browser(feed, true);
+
 			if (!exit_code.has_value()) {
 				v->show_error(_("Failed to spawn browser"));
 				return false;
@@ -184,7 +189,12 @@ bool ItemListFormAction::process_operation(Operation op,
 				"ItemListFormAction: opening all unread items "
 				"in "
 				"browser and marking read");
-			const auto exit_code = open_unread_items_in_browser(feed, true);
+
+			// We can't just `const auto exit_code = ...` here because this
+			// triggers -Wmaybe-initialized in GCC 9 with -O2.
+			nonstd::optional<std::uint8_t> exit_code;
+			exit_code = open_unread_items_in_browser(feed, true);
+
 			if (!exit_code.has_value()) {
 				v->show_error(_("Failed to spawn browser"));
 				return false;

Makefile
@@ -45,7 +45,7 @@ CXX_FOR_BUILD?=$(CXX)
 # compiler and linker flags
 DEFINES=-DLOCALEDIR='"$(localedir)"'
 
-WARNFLAGS=-Werror -Wall -Wextra -Wunreachable-code -Wno-unknown-warning-option -Wno-unknown-pragmas
+WARNFLAGS=-Werror -Wall -Wextra -Wunreachable-code
 INCLUDES=-Iinclude -Istfl -Ifilter -I. -Irss -I$(relative_cargo_target_dir)/cxxbridge/libnewsboat-ffi/src/
 BARE_CXXFLAGS=-std=c++11 -O2 -ggdb $(INCLUDES)
 LDFLAGS+=-L.

src/feedlistformaction.cpp
@@ -229,16 +229,17 @@ REDO:
 					"items in feed at position `%s'",
 					feedpos.c_str());
 
-				const auto exit_code = open_unread_items_in_browser(feed, false);
+				// We can't just `const auto exit_code = ...` here because this
+				// triggers -Wmaybe-initialized in GCC 9 with -O2.
+				nonstd::optional<std::uint8_t> exit_code;
+				exit_code = open_unread_items_in_browser(feed, false);
+
 				if (!exit_code.has_value()) {
 					v->show_error(_("Failed to spawn browser"));
 					return false;
 				} else if (*exit_code != 0) {
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
 					v->show_error(strprintf::fmt(_("Browser returned error code %i"),
 							*exit_code));
-#pragma GCC diagnostic pop
 					return false;
 				}
 			}
@@ -258,15 +259,16 @@ REDO:
 					"marking read",
 					feedpos.c_str());
 
-				const auto exit_code = open_unread_items_in_browser(feed, true);
+				// We can't just `const auto exit_code = ...` here because this
+				// triggers -Wmaybe-initialized in GCC 9 with -O2.
+				nonstd::optional<std::uint8_t> exit_code;
+				exit_code = open_unread_items_in_browser(feed, true);
+
 				if (!exit_code.has_value()) {
 					v->show_error(_("Failed to spawn browser"));
 					return false;
 				} else if (*exit_code != 0) {
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
 					v->show_error(strprintf::fmt(_("Browser returned error code %i"), *exit_code));
-#pragma GCC diagnostic pop
 					return false;
 				}
 

commit 5bdf8665c359ba4abd6abc24796a5cf278bafadb

Fix regression introduced by 54617872ff36d7420505e
A copy-and-paste error in 54617872ff36d7420505e4c82cc08a14ed16f6c3
changed "false" to "true", and so `open-all-unread-in-browser` started
behaving the same as `open-all-unread-in-browser-and-mark-read`.

src/itemlistformaction.cpp
@@ -171,7 +171,7 @@ bool ItemListFormAction::process_operation(Operation op,
 			// We can't just `const auto exit_code = ...` here because this
 			// triggers -Wmaybe-initialized in GCC 9 with -O2.
 			nonstd::optional<std::uint8_t> exit_code;
-			exit_code = open_unread_items_in_browser(feed, true);
+			exit_code = open_unread_items_in_browser(feed, false);
 
 			if (!exit_code.has_value()) {
 				v->show_error(_("Failed to spawn browser"));