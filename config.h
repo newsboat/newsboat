269e8fcdbc51f0e807fcebe80d179eb8a303778d
Alexander Batischev <eual.jp@gmail.com>
2021-02-24 20:20:04 +0300

    Introduce C macro to mark translatable strings
    See the comment in the code for the reason why our existing `_` macro
    won't do. This solution is borrowed from
    https://www.gnu.org/software/gettext/manual/html_node/Special-cases.html

--- config.h
+++ config.h
@@ -27,6 +27,12 @@
 
 #define _s(msg) std::string(gettext(msg))
 
+// Use this macro instead of `_` and `_s` if you want the string to get extracted
+// by xgettext, but don't want to call gettext(). This is useful for
+// initializers of statically-allocated global objects, where gettext() calls
+// operate with default (C) locale and are useless.
+#define translatable(msg) msg
+
 /* #define NDEBUG */ // only enable this #define if you want to disable all debug logging.
 
 #endif /* NEWSBOAT_CONFIG_H_ */
