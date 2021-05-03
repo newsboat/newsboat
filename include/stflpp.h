b46c1ab97faee9508e9694237a8c465eed6366a4
Alexander Batischev <eual.jp@gmail.com>
2021-05-03 16:21:54 +0300

    Turn Stfl into a namespace
    It's a class with a sub-class and two static members — nothing else;
    there is no point for Stfl to be a class, it's really just a namespace.

--- include/stflpp.h
+++ include/stflpp.h
@@ -9,45 +9,46 @@ extern "C" {
 
 namespace newsboat {
 
-class Stfl {
+namespace Stfl {
+
+class Form {
 public:
-	class Form {
-	public:
-		explicit Form(const std::string& text);
-		~Form();
+	explicit Form(const std::string& text);
+	~Form();
 
-		// Make sure this class cannot accidentally get copied.
-		// Copying this class would be bad because the `f` and `ipool` member
-		// variables are managed manually.
-		Form(const Form&) = delete;
-		Form& operator=(const Form&) = delete;
-		// The move operations are not needed at the moment but can be added when necessary.
-		Form(Form&&) = delete;
-		Form& operator=(Form&&) = delete;
+	// Make sure this class cannot accidentally get copied.
+	// Copying this class would be bad because the `f` and `ipool` member
+	// variables are managed manually.
+	Form(const Form&) = delete;
+	Form& operator=(const Form&) = delete;
+	// The move operations are not needed at the moment but can be added when necessary.
+	Form(Form&&) = delete;
+	Form& operator=(Form&&) = delete;
 
-		const char* run(int timeout);
+	const char* run(int timeout);
 
-		std::string get(const std::string& name);
-		void set(const std::string& name, const std::string& value);
+	std::string get(const std::string& name);
+	void set(const std::string& name, const std::string& value);
 
-		std::string get_focus();
-		void set_focus(const std::string& name);
+	std::string get_focus();
+	void set_focus(const std::string& name);
 
-		std::string dump(const std::string& name,
-			const std::string& prefix,
-			int focus);
-		void modify(const std::string& name,
-			const std::string& mode,
-			const std::string& text);
+	std::string dump(const std::string& name,
+		const std::string& prefix,
+		int focus);
+	void modify(const std::string& name,
+		const std::string& mode,
+		const std::string& text);
 
-	private:
-		stfl_form* f;
-		stfl_ipool* ipool;
-	};
-
-	static void reset();
-	static std::string quote(const std::string& text);
+private:
+	stfl_form* f;
+	stfl_ipool* ipool;
 };
+
+void reset();
+std::string quote(const std::string& text);
+
+} // namespace Stfl
 
 } // namespace newsboat
 
