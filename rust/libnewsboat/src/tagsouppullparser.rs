ee867a4f14284051d886856ed79fa12bd3a30c41
Dennis van der Schagt <dennisschagt@gmail.com>
2022-03-12 10:38:25 +0100

    WIP: Implement next()

--- rust/libnewsboat/src/tagsouppullparser.rs
+++ rust/libnewsboat/src/tagsouppullparser.rs
@@ -28,8 +28,27 @@ impl TagSoupPullParser {
         }
     }
 
-    pub fn next() -> Event {
-        panic!("Unimplemented");
+    pub fn next(self: &mut TagSoupPullParser) -> &Event {
+        self.attributes.clear();
+        self.text.clear();
+
+        if self.input.is_empty() {
+            self.current_event = Event::EndDocument;
+        }
+
+        match self.current_event {
+            Event::StartDocument | Event::StartTag | Event::EndTag => {
+                match self.input.chars().next() {
+                    None => self.current_event = Event::EndDocument,
+                    Some('<') => self.handle_tag(),
+                    Some(c) => self.handle_text(c),
+                }
+            }
+            Event::Text => self.handle_tag(),
+            Event::EndDocument => {}
+        }
+
+        self.get_event_type()
     }
 
     pub fn get_attribute_value(self: &TagSoupPullParser, name: &str) -> Option<&String> {
@@ -45,5 +64,13 @@ impl TagSoupPullParser {
 
     pub fn get_text(self: &TagSoupPullParser) -> &str {
         &self.text
+    }
+
+    fn handle_tag(self: &TagSoupPullParser) {
+        panic!("Unimplemented");
+    }
+
+    fn handle_text(self: &TagSoupPullParser, c: char) {
+        panic!("Unimplemented");
     }
 }
