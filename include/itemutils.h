114381320ec6915e2bb6a1dbd8b57978523c4ae3
Dennis van der Schagt <dennisschagt@gmail.com>
2022-05-28 12:35:23 +0200

    Add option to enqueue enclosure URLs from article list

---
+++ include/itemutils.h
@@ -0,0 +1,24 @@
+#ifndef NEWSBOAT_ITEMUTILS_H_
+#define NEWSBOAT_ITEMUTILS_H_
+
+#include <memory>
+
+#include "cache.h"
+#include "rssfeed.h"
+#include "rssitem.h"
+#include "view.h"
+
+namespace newsboat {
+
+/// Adds enclosure URL of item to queue.
+///
+/// Reports outcome on the statusline.
+///
+/// Returns true if URL was successfully added to queue or was already queued before,
+///         false otherwise.
+bool EnqueueItemEnclosure(std::shared_ptr<RssItem> item, std::shared_ptr<RssFeed> feed,
+	View& v, Cache& cache);
+
+} // namespace newsboat
+
+#endif /* NEWSBOAT_ITEMUTILS_H_ */
--- src/itemviewformaction.cpp
+++ src/itemviewformaction.cpp
@@ -10,6 +10,7 @@
 #include "fmtstrformatter.h"
 #include "itemlistformaction.h"
 #include "itemrenderer.h"
+#include "itemutils.h"
 #include "htmlrenderer.h"
 #include "logger.h"
 #include "rssfeed.h"
@@ -202,41 +203,8 @@ bool ItemViewFormAction::process_operation(Operation op,
 		show_source = !show_source;
 		do_redraw = true;
 		break;
-	case OP_ENQUEUE: {
-		if (item->enclosure_url().empty()) {
-			v->get_statusline().show_error(_("Item has no enclosures."));
-			return false;
-		} else if (!utils::is_http_url(item->enclosure_url())) {
-			v->get_statusline().show_error(strprintf::fmt(
-					_("Item's enclosure has non-http link: '%s'"), item->enclosure_url()));
-			return false;
-		} else {
-			const EnqueueResult result = v->get_ctrl()->enqueue_url(item, feed);
-			rsscache->update_rssitem_unread_and_enqueued(item, feed->rssurl());
-			switch (result.status) {
-			case EnqueueStatus::QUEUED_SUCCESSFULLY:
-				v->get_statusline().show_message(
-					strprintf::fmt(_("Added %s to download queue."),
-						item->enclosure_url()));
-				return true;
-			case EnqueueStatus::URL_QUEUED_ALREADY:
-				v->get_statusline().show_message(
-					strprintf::fmt(_("%s is already queued."),
-						item->enclosure_url()));
-				return true; // Not a failure, just an idempotent action
-			case EnqueueStatus::OUTPUT_FILENAME_USED_ALREADY:
-				v->get_statusline().show_error(
-					strprintf::fmt(_("Generated filename (%s) is used already."),
-						result.extra_info));
-				return false;
-			case EnqueueStatus::QUEUE_FILE_OPEN_ERROR:
-				v->get_statusline().show_error(
-					strprintf::fmt(_("Failed to open queue file: %s."), result.extra_info));
-				return false;
-			}
-		}
-	}
-	break;
+	case OP_ENQUEUE:
+		return EnqueueItemEnclosure(item, feed, *v, *rsscache);
 	case OP_SAVE: {
 		LOG(Level::INFO, "ItemViewFormAction::process_operation: saving article");
 		std::string filename;
