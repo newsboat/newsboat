4ce8693e32d6d9b15394cc7f0310584dfbe6fe5e
Alexander Batischev <eual.jp@gmail.com>
2023-07-22 17:39:48 +0300

    Migrate test_helpers::MainTempDir to Filepath

--- test/test_helpers/maintempdir.cpp
+++ test/test_helpers/maintempdir.cpp
@@ -7,8 +7,9 @@
 #include <unistd.h>
 
 test_helpers::MainTempDir::tempfileexception::tempfileexception(
+	const newsboat::Filepath& filepath,
 	const std::string& error)
-	: msg("tempfileexception: " + error)
+	: msg("tempfileexception: " + filepath.display() + ": " + error)
 {
 };
 
@@ -22,14 +23,15 @@ test_helpers::MainTempDir::MainTempDir()
 	char* tmpdir_p = ::getenv("TMPDIR");
 
 	if (tmpdir_p) {
-		tempdir = tmpdir_p;
+		tempdir = newsboat::Filepath::from_locale_string(tmpdir_p);
 	} else {
-		tempdir = "/tmp/";
+		tempdir = newsboat::Filepath::from_locale_string("/tmp");
 	}
 
-	tempdir += "/newsboat-tests/";
+	tempdir.push(newsboat::Filepath::from_locale_string("newsboat-tests"));
 
-	int status = mkdir(tempdir.c_str(), S_IRWXU);
+	const auto tempdir_str = tempdir.to_locale_string();
+	int status = mkdir(tempdir_str.c_str(), S_IRWXU);
 	if (status != 0) {
 		// The directory already exists. That's fine, though,
 		// but only as long as it has all the properties we
@@ -40,7 +42,7 @@ test_helpers::MainTempDir::MainTempDir()
 
 		if (saved_errno == EEXIST) {
 			struct stat buffer;
-			if (lstat(tempdir.c_str(), &buffer) == 0) {
+			if (lstat(tempdir_str.c_str(), &buffer) == 0) {
 				if (buffer.st_mode & S_IRUSR &&
 					buffer.st_mode & S_IWUSR &&
 					buffer.st_mode & S_IXUSR) {
@@ -50,7 +52,7 @@ test_helpers::MainTempDir::MainTempDir()
 		}
 
 		if (!success) {
-			throw tempfileexception(strerror(saved_errno));
+			throw tempfileexception(tempdir, strerror(saved_errno));
 		}
 	}
 }
@@ -59,10 +61,13 @@ test_helpers::MainTempDir::~MainTempDir()
 {
 	// Try to remove the tempdir, but don't try *too* hard: there might be
 	// other objects still using it. The last one will hopefully delete it.
-	::rmdir(tempdir.c_str());
+	const auto tempdir_str = tempdir.to_locale_string();
+	::rmdir(tempdir_str.c_str());
 }
 
 const std::string test_helpers::MainTempDir::get_path() const
 {
-	return tempdir;
+	auto result = tempdir.to_locale_string();
+	result.push_back('/');
+	return result;
 }
