5fcad7aa9fdc01959129f1b4c45aabede43c7a45
bogdasar1985 <bogdasar1985@gmail.com>
2023-10-02 16:05:17 +0500

    Migrate TtRssUrlReader to Filepath

--- include/ttrssurlreader.h
+++ include/ttrssurlreader.h
@@ -9,13 +9,13 @@ class RemoteApi;
 
 class TtRssUrlReader : public UrlReader {
 public:
-	TtRssUrlReader(const std::string& url_file, RemoteApi* a);
+	TtRssUrlReader(const Filepath& url_file, RemoteApi* a);
 	~TtRssUrlReader() override;
 	nonstd::optional<utils::ReadTextFileError> reload() override;
 	std::string get_source() override;
 
 private:
-	std::string file;
+	Filepath file;
 	RemoteApi* api;
 };
 
