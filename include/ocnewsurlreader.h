8df80cc631b67e15d893a941dd4346da96be5c12
bogdasar1985 <bogdasar1985@gmail.com>
2023-10-03 12:45:21 +0500

    Migrate OcNewsUrlReader to Filepath

--- include/ocnewsurlreader.h
+++ include/ocnewsurlreader.h
@@ -9,13 +9,13 @@ class RemoteApi;
 
 class OcNewsUrlReader : public UrlReader {
 public:
-	OcNewsUrlReader(const std::string& url_file, RemoteApi* a);
+	OcNewsUrlReader(const Filepath& url_file, RemoteApi* a);
 	~OcNewsUrlReader() override;
 	nonstd::optional<utils::ReadTextFileError> reload() override;
 	std::string get_source() override;
 
 private:
-	std::string file;
+	Filepath file;
 	RemoteApi* api;
 };
 
