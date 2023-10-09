6cd5623d7e1103657b0f0b04c8cde65341bc5253
bogdasar1985 <bogdasar1985@gmail.com>
2023-10-09 15:12:36 +0500

    Migrate FreshRssUrlReader to Filepath

--- src/freshrssurlreader.cpp
+++ src/freshrssurlreader.cpp
@@ -10,10 +10,10 @@
 namespace newsboat {
 
 FreshRssUrlReader::FreshRssUrlReader(ConfigContainer* c,
-	const std::string& url_file,
+	const Filepath& url_file,
 	RemoteApi* a)
 	: cfg(c)
-	, file(url_file)
+	, file(url_file.clone())
 	, api(a)
 {
 }
