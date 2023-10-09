86326d7bd7e3fb741e8e4441bcdc632aceffdf29
bogdasar1985 <bogdasar1985@gmail.com>
2023-10-09 15:10:15 +0500

    Migrate History to Filepath

--- src/history.cpp
+++ src/history.cpp
@@ -22,14 +22,14 @@ std::string History::next_line()
 	return std::string(history::bridged::next_line(*rs_object));
 }
 
-void History::load_from_file(const std::string& file)
+void History::load_from_file(const Filepath& file)
 {
-	history::bridged::load_from_file(*rs_object, file);
+	history::bridged::load_from_file(*rs_object, file.to_locale_string());
 }
 
-void History::save_to_file(const std::string& file, unsigned int limit)
+void History::save_to_file(const Filepath& file, unsigned int limit)
 {
-	history::bridged::save_to_file(*rs_object, file, limit);
+	history::bridged::save_to_file(*rs_object, file.to_locale_string(), limit);
 }
 
 } // namespace newsboat
