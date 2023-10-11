0ba2773ff123c236c9a855b18e4dfc404126df2b
bogdasar1985 <bogdasar1985@gmail.com>
2023-10-11 15:24:03 +0500

    Migrate Download to Filepath

--- include/download.h
+++ include/download.h
@@ -4,6 +4,8 @@
 #include <functional>
 #include <string>
 
+#include "filepath.h"
+
 namespace podboat {
 
 enum class DlStatus {
@@ -22,6 +24,8 @@ enum class DlStatus {
 class Download {
 public:
 	explicit Download(std::function<void()> cb_require_view_update);
+	Download(const Download& download);
+	Download& operator=(const Download& download);
 	double percents_finished() const;
 	const std::string status_text() const;
 	DlStatus status() const
@@ -32,10 +36,10 @@ public:
 	{
 		return msg;
 	}
-	const std::string& filename() const;
-	const std::string basename() const;
+	const newsboat::Filepath filename() const;
+	const newsboat::Filepath basename() const;
 	const std::string& url() const;
-	void set_filename(const std::string& str);
+	void set_filename(const newsboat::Filepath& str);
 	void set_url(const std::string& url);
 	void set_progress(double downloaded, double total);
 	void set_status(DlStatus dls, const std::string& msg_ = {});
@@ -53,7 +57,7 @@ public:
 	}
 
 private:
-	std::string fn;
+	newsboat::Filepath fn;
 	std::string url_;
 	DlStatus download_status;
 	std::string msg;
