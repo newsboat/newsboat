cf5fb297ab710b1933cde359d2b17fd0289b3a20
Dennis van der Schagt <dennisschagt@gmail.com>
2024-01-06 15:39:34 +0100

    Use PathBuf/Filepath on ConfigPaths FFI

--- rust/libnewsboat-ffi/src/configpaths.rs
+++ rust/libnewsboat-ffi/src/configpaths.rs
@@ -1,6 +1,7 @@
 use crate::cliargsparser::CliArgsParser;
+use crate::filepath::PathBuf;
 use libnewsboat::configpaths;
-use std::path::Path;
+use std::pin::Pin;
 
 // cxx doesn't allow to share types from other crates, so we have to wrap it
 // cf. https://github.com/dtolnay/cxx/issues/496
@@ -12,6 +13,11 @@ mod bridged {
     extern "C++" {
         include!("libnewsboat-ffi/src/cliargsparser.rs.h");
         type CliArgsParser = crate::cliargsparser::CliArgsParser;
+    }
+    #[namespace = "newsboat::filepath::bridged"]
+    extern "C++" {
+        include!("libnewsboat-ffi/src/filepath.rs.h");
+        type PathBuf = crate::filepath::PathBuf;
     }
 
     extern "Rust" {
@@ -28,14 +34,14 @@ mod bridged {
 
         fn try_migrate_from_newsbeuter(configpaths: &mut ConfigPaths) -> bool;
 
-        fn url_file(configpaths: &ConfigPaths) -> String;
-        fn cache_file(configpaths: &ConfigPaths) -> String;
-        fn set_cache_file(configpaths: &mut ConfigPaths, path: &str);
-        fn config_file(configpaths: &ConfigPaths) -> String;
-        fn lock_file(configpaths: &ConfigPaths) -> String;
-        fn queue_file(configpaths: &ConfigPaths) -> String;
-        fn search_history_file(configpaths: &ConfigPaths) -> String;
-        fn cmdline_history_file(configpaths: &ConfigPaths) -> String;
+        fn url_file(configpaths: &ConfigPaths, mut path: Pin<&mut PathBuf>);
+        fn cache_file(configpaths: &ConfigPaths, mut path: Pin<&mut PathBuf>);
+        fn set_cache_file(configpaths: &mut ConfigPaths, path: &PathBuf);
+        fn config_file(configpaths: &ConfigPaths, mut path: Pin<&mut PathBuf>);
+        fn lock_file(configpaths: &ConfigPaths, mut path: Pin<&mut PathBuf>);
+        fn queue_file(configpaths: &ConfigPaths, mut path: Pin<&mut PathBuf>);
+        fn search_history_file(configpaths: &ConfigPaths, mut path: Pin<&mut PathBuf>);
+        fn cmdline_history_file(configpaths: &ConfigPaths, mut path: Pin<&mut PathBuf>);
     }
 }
 
@@ -63,42 +69,34 @@ fn try_migrate_from_newsbeuter(configpaths: &mut ConfigPaths) -> bool {
     configpaths.0.try_migrate_from_newsbeuter()
 }
 
-fn url_file(configpaths: &ConfigPaths) -> String {
-    configpaths.0.url_file().to_string_lossy().into_owned()
+fn url_file(configpaths: &ConfigPaths, mut path: Pin<&mut PathBuf>) {
+    path.0 = configpaths.0.url_file().to_owned();
 }
 
-fn cache_file(configpaths: &ConfigPaths) -> String {
-    configpaths.0.cache_file().to_string_lossy().into_owned()
+fn cache_file(configpaths: &ConfigPaths, mut path: Pin<&mut PathBuf>) {
+    path.0 = configpaths.0.cache_file().to_owned();
 }
 
-fn set_cache_file(configpaths: &mut ConfigPaths, path: &str) {
-    configpaths.0.set_cache_file(Path::new(path).to_owned());
+fn set_cache_file(configpaths: &mut ConfigPaths, path: &PathBuf) {
+    configpaths.0.set_cache_file(path.0.to_owned());
 }
 
-fn config_file(configpaths: &ConfigPaths) -> String {
-    configpaths.0.config_file().to_string_lossy().into_owned()
+fn config_file(configpaths: &ConfigPaths, mut path: Pin<&mut PathBuf>) {
+    path.0 = configpaths.0.config_file().to_owned();
 }
 
-fn lock_file(configpaths: &ConfigPaths) -> String {
-    configpaths.0.lock_file().to_string_lossy().into_owned()
+fn lock_file(configpaths: &ConfigPaths, mut path: Pin<&mut PathBuf>) {
+    path.0 = configpaths.0.lock_file().to_owned();
 }
 
-fn queue_file(configpaths: &ConfigPaths) -> String {
-    configpaths.0.queue_file().to_string_lossy().into_owned()
+fn queue_file(configpaths: &ConfigPaths, mut path: Pin<&mut PathBuf>) {
+    path.0 = configpaths.0.queue_file().to_owned();
 }
 
-fn search_history_file(configpaths: &ConfigPaths) -> String {
-    configpaths
-        .0
-        .search_history_file()
-        .to_string_lossy()
-        .into_owned()
+fn search_history_file(configpaths: &ConfigPaths, mut path: Pin<&mut PathBuf>) {
+    path.0 = configpaths.0.search_history_file().to_owned();
 }
 
-fn cmdline_history_file(configpaths: &ConfigPaths) -> String {
-    configpaths
-        .0
-        .cmdline_history_file()
-        .to_string_lossy()
-        .into_owned()
+fn cmdline_history_file(configpaths: &ConfigPaths, mut path: Pin<&mut PathBuf>) {
+    path.0 = configpaths.0.cmdline_history_file().to_owned();
 }
