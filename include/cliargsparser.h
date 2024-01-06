ee5e9997e21740e7ca2a7cf6460b89af98bc7dc5
Dennis van der Schagt <dennisschagt@gmail.com>
2024-01-06 14:00:04 +0100

    Use PathBuf/Filepath on CliArgsParser FFI

--- include/cliargsparser.h
+++ include/cliargsparser.h
@@ -72,11 +72,11 @@ public:
 
 	nonstd::optional<Filepath> config_file() const;
 
-	nonstd::optional<std::string> queue_file() const;
+	nonstd::optional<Filepath> queue_file() const;
 
-	nonstd::optional<std::string> search_history_file() const;
+	nonstd::optional<Filepath> search_history_file() const;
 
-	nonstd::optional<std::string> cmdline_history_file() const;
+	nonstd::optional<Filepath> cmdline_history_file() const;
 
 	/// If non-empty, Newsboat should execute these commands and then quit.
 	///
