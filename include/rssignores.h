44b3959514766ee3ca9ab00b67b9bd0d79e897d1
Daniel Oh <danieloh0714@gmail.com>
2024-03-16 03:24:44 -0500

    fmt files

--- include/rssignores.h
+++ include/rssignores.h
@@ -13,23 +13,23 @@ namespace newsboat {
 
 class RssIgnores : public ConfigActionHandler {
 public:
-	RssIgnores() {}
-	~RssIgnores() override {}
-	void handle_action(const std::string& action,
-		const std::vector<std::string>& params) override;
-	void dump_config(std::vector<std::string>& config_output) const override;
-	bool matches(RssItem* item);
-	bool matches_lastmodified(const std::string& url);
-	bool matches_resetunread(const std::string& url);
+    RssIgnores() {}
+    ~RssIgnores() override {}
+    void handle_action(const std::string& action,
+                       const std::vector<std::string>& params) override;
+    void dump_config(std::vector<std::string>& config_output) const override;
+    bool matches(RssItem* item);
+    bool matches_lastmodified(const std::string& url);
+    bool matches_resetunread(const std::string& url);
 
 private:
-	bool matches_expr(std::shared_ptr<Matcher> expr, RssItem* item);
+    bool matches_expr(std::shared_ptr<Matcher> expr, RssItem* item);
 
-	std::unordered_multimap<std::string, std::shared_ptr<Matcher>> ignores;
-	std::vector<std::string> ignores_lastmodified;
-	std::vector<std::string> resetflag;
+    std::unordered_multimap<std::string, std::shared_ptr<Matcher>> ignores;
+    std::vector<std::string> ignores_lastmodified;
+    std::vector<std::string> resetflag;
 
-	static const std::string REGEX_PREFIX;
+    static const std::string REGEX_PREFIX;
 };
 
 } // namespace newsboat
--- src/rssignores.cpp
+++ src/rssignores.cpp
@@ -31,143 +31,143 @@ namespace newsboat {
 const std::string RssIgnores::REGEX_PREFIX = "regex:";
 
 void RssIgnores::handle_action(const std::string& action,
-	const std::vector<std::string>& params)
+                               const std::vector<std::string>& params)
 {
-	if (action == "ignore-article") {
-		if (params.size() < 2) {
-			throw ConfigHandlerException(ActionHandlerStatus::TOO_FEW_PARAMS);
-		}
-		std::string ignore_rssurl = params[0];
-		std::string ignore_expr = params[1];
-		auto m = std::make_shared<Matcher>();
-		if (!m->parse(ignore_expr)) {
-			throw ConfigHandlerException(strprintf::fmt(
-					_("couldn't parse filter expression `%s': %s"),
-					ignore_expr,
-					m->get_parse_error()));
-		}
+    if (action == "ignore-article") {
+        if (params.size() < 2) {
+            throw ConfigHandlerException(ActionHandlerStatus::TOO_FEW_PARAMS);
+        }
+        std::string ignore_rssurl = params[0];
+        std::string ignore_expr = params[1];
+        auto m = std::make_shared<Matcher>();
+        if (!m->parse(ignore_expr)) {
+            throw ConfigHandlerException(strprintf::fmt(
+                                             _("couldn't parse filter expression `%s': %s"),
+                                             ignore_expr,
+                                             m->get_parse_error()));
+        }
 
-		const int prefix_len = REGEX_PREFIX.length();
-		if (!ignore_rssurl.compare(0, prefix_len, REGEX_PREFIX)) {
-			std::string errorMessage;
-			const std::string pattern = ignore_rssurl.substr(prefix_len,
-					ignore_rssurl.length() - prefix_len);
-			const auto regex = Regex::compile(pattern, REG_EXTENDED | REG_ICASE, errorMessage);
-			if (regex == nullptr) {
-				throw ConfigHandlerException(strprintf::fmt(
-						_("`%s' is not a valid regular expression: %s"),
-						pattern, errorMessage));
-			}
-		}
+        const int prefix_len = REGEX_PREFIX.length();
+        if (!ignore_rssurl.compare(0, prefix_len, REGEX_PREFIX)) {
+            std::string errorMessage;
+            const std::string pattern = ignore_rssurl.substr(prefix_len,
+                                        ignore_rssurl.length() - prefix_len);
+            const auto regex = Regex::compile(pattern, REG_EXTENDED | REG_ICASE, errorMessage);
+            if (regex == nullptr) {
+                throw ConfigHandlerException(strprintf::fmt(
+                                                 _("`%s' is not a valid regular expression: %s"),
+                                                 pattern, errorMessage));
+            }
+        }
 
-		ignores.insert({ignore_rssurl, m});
-	} else if (action == "always-download") {
-		if (params.empty()) {
-			throw ConfigHandlerException(ActionHandlerStatus::TOO_FEW_PARAMS);
-		}
+        ignores.insert({ignore_rssurl, m});
+    } else if (action == "always-download") {
+        if (params.empty()) {
+            throw ConfigHandlerException(ActionHandlerStatus::TOO_FEW_PARAMS);
+        }
 
-		for (const auto& param : params) {
-			ignores_lastmodified.push_back(param);
-		}
-	} else if (action == "reset-unread-on-update") {
-		if (params.empty()) {
-			throw ConfigHandlerException(ActionHandlerStatus::TOO_FEW_PARAMS);
-		}
+        for (const auto& param : params) {
+            ignores_lastmodified.push_back(param);
+        }
+    } else if (action == "reset-unread-on-update") {
+        if (params.empty()) {
+            throw ConfigHandlerException(ActionHandlerStatus::TOO_FEW_PARAMS);
+        }
 
-		for (const auto& param : params) {
-			resetflag.push_back(param);
-		}
-	} else {
-		throw ConfigHandlerException(ActionHandlerStatus::INVALID_COMMAND);
-	}
+        for (const auto& param : params) {
+            resetflag.push_back(param);
+        }
+    } else {
+        throw ConfigHandlerException(ActionHandlerStatus::INVALID_COMMAND);
+    }
 }
 
 void RssIgnores::dump_config(std::vector<std::string>& config_output) const
 {
-	for (const auto& ign : ignores) {
-		std::string configline = "ignore-article ";
-		if (ign.first == "*") {
-			configline.append("*");
-		} else {
-			configline.append(utils::quote(ign.first));
-		}
-		configline.append(" ");
-		configline.append(utils::quote(ign.second->get_expression()));
-		config_output.push_back(configline);
-	}
-	for (const auto& ign_lm : ignores_lastmodified) {
-		config_output.push_back(strprintf::fmt(
-				"always-download %s", utils::quote(ign_lm)));
-	}
-	for (const auto& rf : resetflag) {
-		config_output.push_back(strprintf::fmt(
-				"reset-unread-on-update %s", utils::quote(rf)));
-	}
+    for (const auto& ign : ignores) {
+        std::string configline = "ignore-article ";
+        if (ign.first == "*") {
+            configline.append("*");
+        } else {
+            configline.append(utils::quote(ign.first));
+        }
+        configline.append(" ");
+        configline.append(utils::quote(ign.second->get_expression()));
+        config_output.push_back(configline);
+    }
+    for (const auto& ign_lm : ignores_lastmodified) {
+        config_output.push_back(strprintf::fmt(
+                                    "always-download %s", utils::quote(ign_lm)));
+    }
+    for (const auto& rf : resetflag) {
+        config_output.push_back(strprintf::fmt(
+                                    "reset-unread-on-update %s", utils::quote(rf)));
+    }
 }
 
 bool RssIgnores::matches_expr(std::shared_ptr<Matcher> expr, RssItem *item)
 {
-	if (expr->matches(item)) {
-		LOG(Level::DEBUG,
-			"RssIgnores::matches_expr: found match");
-		return true;
-	}
+    if (expr->matches(item)) {
+        LOG(Level::DEBUG,
+            "RssIgnores::matches_expr: found match");
+        return true;
+    }
 
-	return false;
+    return false;
 }
 
 bool RssIgnores::matches(RssItem* item)
 {
-	auto search = ignores.equal_range(item->feedurl());
-	for (auto itr = search.first; itr != search.second; itr++) {
-		if (matches_expr(itr->second, item))
-			return true;
-	}
+    auto search = ignores.equal_range(item->feedurl());
+    for (auto itr = search.first; itr != search.second; itr++) {
+        if (matches_expr(itr->second, item))
+            return true;
+    }
 
-	search = ignores.equal_range("*");
-	for (auto itr = search.first; itr != search.second; itr++) {
-		if (matches_expr(itr->second, item))
-			return true;
-	}
+    search = ignores.equal_range("*");
+    for (auto itr = search.first; itr != search.second; itr++) {
+        if (matches_expr(itr->second, item))
+            return true;
+    }
 
-	const int prefix_len = REGEX_PREFIX.length();
-	for (const auto& ign : ignores) {
-		LOG(Level::DEBUG,
-			"RssIgnores::matches: ign.first = `%s' item->feedurl = `%s'",
-			ign.first,
-			item->feedurl());
+    const int prefix_len = REGEX_PREFIX.length();
+    for (const auto& ign : ignores) {
+        LOG(Level::DEBUG,
+            "RssIgnores::matches: ign.first = `%s' item->feedurl = `%s'",
+            ign.first,
+            item->feedurl());
 
-		if (!ign.first.compare(0, prefix_len, REGEX_PREFIX)) {
-			const std::string pattern = ign.first.substr(prefix_len, ign.first.length() - prefix_len);
-			std::string errorMessage;
-			const auto regex = Regex::compile(pattern, REG_EXTENDED | REG_ICASE, errorMessage);
-			const auto matches = regex->matches(item->feedurl(), 1, 0);
-			if (!matches.empty() && matches_expr(ign.second, item))
-				return true;
-		}
-	}
+        if (!ign.first.compare(0, prefix_len, REGEX_PREFIX)) {
+            const std::string pattern = ign.first.substr(prefix_len, ign.first.length() - prefix_len);
+            std::string errorMessage;
+            const auto regex = Regex::compile(pattern, REG_EXTENDED | REG_ICASE, errorMessage);
+            const auto matches = regex->matches(item->feedurl(), 1, 0);
+            if (!matches.empty() && matches_expr(ign.second, item))
+                return true;
+        }
+    }
 
-	return false;
+    return false;
 }
 
 bool RssIgnores::matches_lastmodified(const std::string& url)
 {
-	return std::find_if(ignores_lastmodified.begin(),
-			ignores_lastmodified.end(),
-	[&](const std::string& u) {
-		return u == url;
-	}) !=
-	ignores_lastmodified.end();
+    return std::find_if(ignores_lastmodified.begin(),
+                        ignores_lastmodified.end(),
+    [&](const std::string& u) {
+        return u == url;
+    }) !=
+    ignores_lastmodified.end();
 }
 
 bool RssIgnores::matches_resetunread(const std::string& url)
 {
-	return std::find_if(resetflag.begin(),
-			resetflag.end(),
-	[&](const std::string& u) {
-		return u == url;
-	}) !=
-	resetflag.end();
+    return std::find_if(resetflag.begin(),
+                        resetflag.end(),
+    [&](const std::string& u) {
+        return u == url;
+    }) !=
+    resetflag.end();
 }
 
 } // namespace newsboat
