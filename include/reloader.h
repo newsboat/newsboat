8777780a3036c79dcf8063830eea9cdbbf0a3766
Juho Eerola <juho.eerola@tuni.fi>
2024-06-27 16:31:44 +0300

    Extract common reloading logic to method

--- include/reloader.h
+++ include/reloader.h
@@ -53,13 +53,8 @@ private:
 	void reload_indexes(const std::vector<unsigned int>& indexes,
 		bool unattended = false);
 
-	/// \brief Reloads feeds occupying positions from \a start to \a end in
-	/// feedlist.
-	///
-	/// Only updates status bar if \a unattended is false.
-	void reload_range(unsigned int start,
-		unsigned int end,
-		bool unattended = false);
+	void reload_indexes_impl(std::vector<unsigned int> indexes,
+		bool unattended);
 
 	/// \brief Reloads given feed.
 	///
