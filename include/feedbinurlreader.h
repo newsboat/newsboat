885b7db009188fe5d3760cc68c5ea3873cc7e040
Dennis van der Schagt <dennisschagt@gmail.com>
2024-07-06 13:39:19 +0200

    Use filepath in the feedbin url reader

--- include/feedbinurlreader.h
+++ include/feedbinurlreader.h
@@ -1,6 +1,7 @@
 #ifndef NEWSBOAT_FEEDBINURLREADER_H_
 #define NEWSBOAT_FEEDBINURLREADER_H_
 
+#include "filepath.h"
 #include "urlreader.h"
 
 namespace newsboat {
@@ -9,12 +10,12 @@ class RemoteApi;
 
 class FeedbinUrlReader : public UrlReader {
 public:
-	FeedbinUrlReader(const std::string& url_file, RemoteApi* a);
+	FeedbinUrlReader(const Filepath& url_file, RemoteApi* a);
 	std::optional<utils::ReadTextFileError> reload() override;
 	std::string get_source() override;
 
 private:
-	std::string file;
+	Filepath file;
 	RemoteApi* api;
 };
 
