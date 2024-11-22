3c49dc78899ac4511c844e88a0b26a7c9e4beabc
Anshul Gupta <ansg191@anshulg.com>
2024-11-22 12:08:38 -0800

    Add support for enclosures from Miniflux
    The enclosures are retrieved from the `enclosures` field of the existing
    JSON response from `Get Feed Entries`.
    
    The API JSON format for an enclosure is specified here:
    https://miniflux.app/docs/api.html#endpoint-get-enclosure

--- src/minifluxapi.cpp
+++ src/minifluxapi.cpp
@@ -235,6 +235,22 @@ rsspp::Feed MinifluxApi::fetch_feed(const std::string& id, CurlHandle& cached_ha
 				item.content_encoded = entry["content"];
 			}
 
+			if (!entry["enclosures"].is_null() && entry["enclosures"].is_array()) {
+				for (const auto& enclosure : entry["enclosures"]) {
+					rsspp::Enclosure enc;
+
+					if (!enclosure["url"].is_null()) {
+						enc.url = enclosure["url"];
+					}
+
+					if (!enclosure["mime_type"].is_null()) {
+						enc.type = enclosure["mime_type"];
+					}
+
+					item.enclosures.push_back(enc);
+				}
+			}
+
 			const int entry_id = entry["id"];
 			item.guid = std::to_string(entry_id);
 
