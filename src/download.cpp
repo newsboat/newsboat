c4be1b361f328edc5fe8932b6200a44233fed504
Alexander Batischev <eual.jp@gmail.com>
2025-08-24 18:36:38 +0300

    Avoid repeated calls to file_name() on same object

--- src/download.cpp
+++ src/download.cpp
@@ -28,7 +28,8 @@ newsboat::Filepath Download::filename() const
 
 newsboat::Filepath Download::basename() const
 {
-	return fn.file_name().has_value() ? fn.file_name().value() : newsboat::Filepath();
+	const auto file_name = fn.file_name();
+	return file_name.has_value() ? file_name.value() : newsboat::Filepath();
 }
 
 const std::string& Download::url() const
