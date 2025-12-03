cdc58924c0220f03fc08b08815709d99ad845a58
Kamil Wsół <kamil.wsol@gmail.com>
2025-12-03 10:29:28 +0100

    Measure loading feeds from cache

--- src/controller.cpp
+++ src/controller.cpp
@@ -452,33 +452,37 @@ int Controller::run(const CliArgsParser& args)
 	}
 	std::cout.flush();
 
-	unsigned int i = 0;
-	for (const auto& url : urlcfg->get_urls()) {
-		try {
-			bool ignore_disp =
-				(cfg.get_configvalue("ignore-mode") ==
-					"display");
-			std::shared_ptr<RssFeed> feed =
-				rsscache->internalize_rssfeed(
-					url, ignore_disp ? &ign : nullptr);
-			feed->set_tags(urlcfg->get_tags(url));
-			feed->set_order(i);
-			feedcontainer.add_feed(feed);
-		} catch (const DbException& e) {
-			std::cerr << _("Error while loading feeds from "
-					"database: ")
-				<< e.what() << std::endl;
-			return EXIT_FAILURE;
-		} catch (const std::string& str) {
-			std::cerr << strprintf::fmt(
-					_("Error while loading feed '%s': "
-						"%s"),
-					url,
-					str)
-				<< std::endl;
-			return EXIT_FAILURE;
+	{
+		ScopeMeasure m1("Load articles from cache");
+
+		unsigned int i = 0;
+		for (const auto& url : urlcfg->get_urls()) {
+			try {
+				bool ignore_disp =
+					(cfg.get_configvalue("ignore-mode") ==
+					 "display");
+				std::shared_ptr<RssFeed> feed =
+					rsscache->internalize_rssfeed(
+							url, ignore_disp ? &ign : nullptr);
+				feed->set_tags(urlcfg->get_tags(url));
+				feed->set_order(i);
+				feedcontainer.add_feed(feed);
+			} catch (const DbException& e) {
+				std::cerr << _("Error while loading feeds from "
+						"database: ")
+					<< e.what() << std::endl;
+				return EXIT_FAILURE;
+			} catch (const std::string& str) {
+				std::cerr << strprintf::fmt(
+						_("Error while loading feed '%s': "
+							"%s"),
+						url,
+						str)
+					<< std::endl;
+				return EXIT_FAILURE;
+			}
+			i++;
 		}
-		i++;
 	}
 
 	if (!args.do_export() && !args.silent()) {
